2c034c9713950b51bb8c2c5c0d859290b024d02c
Larry Nguyen <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-02-15 15:13:14 -0500

    Layout container created with routing support.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { hot } from "react-hot-loader/root";
+import 'antd/dist/antd.css';
+
 import './App.css'
-import logo from './assets/logo.svg'
-import img1 from './assets/img1.png'
-import img2 from './assets/img2.png'
-import img3 from './assets/img3.png'
+import Layout from './hoc/Layout/Layout';
+import Home from './containers/Home/Home';
 
 function App() {
 
   return (
     <>
-    <div className="app-container">
-      <div className="heading">
-        <div className="logo-container">
-          <img src={logo} className="app-logo" alt="logo" />
-        </div>
-        <div className="heading-title">
-          React boilerplate for ServiceNow applications
-        </div>
-      </div>
-      <div className="text-block">
-        <p>The boilerplate designed to build React applications specifically for ServiceNow deployment.</p>
-        <p>Check <code>README.md</code> for detailed overview and step-by-step instructions.</p>
-      </div>
-    </div>
-      <div className="footer">
-        <div>built with love for ServiceNow community</div>
-        <div><img src={img1} alt="image1"/></div>
-        <div><img src={img2} alt="image2"/></div>
-        <div><img src={img3} alt="image3"/></div>
-      </div>
-  </>
+      <Layout>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Layout>
+    </>
   );
 }
 
