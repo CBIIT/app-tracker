eba58b3f1361df5e17bca130aa4c83ff010acca3
Nick Ruggieri <nicolasruggieri7@gmail.com>
2021-02-16 11:26:45 -0500

    changed folder names

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 
-import './App.css'
-import Layout from './hoc/Layout/Layout';
-import Home from './containers/Home/Home';
+import "./App.css";
+import Layout from "./hoc/Layout/Layout";
+import Home from "./Containers/Home/Home";
 
 function App() {
-
   return (
     <>
       <Layout>
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Layout>
     </>
