2a72297eacf4353d6e0fde4305b58224d2b0b0be
Larry Nguyen <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-02-18 17:59:45 -0500

    Create vacancy button.  Temp login link.

--- src/components/Header/NavigationItems/NavigationItems.js
+++ src/components/Header/NavigationItems/NavigationItems.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import NavigationItem from './NavigationItem/NavigationItem';
-import './NavigationItems.css';
+import NavigationItem from "./NavigationItem/NavigationItem";
+import "./NavigationItems.css";
 
 const navigationItems = () => (
-    <div className='NavigationContainer'>
-        <nav>
-            <ul className='NavigationItems'>
-                <NavigationItem link='/'>Executive Careers</NavigationItem>
-                <NavigationItem link='/create-vacancy'>The NCI Hiring Experience</NavigationItem>
-            </ul>
-        </nav>
-    </div>
+  <div className="NavigationContainer">
+    <nav>
+      <ul className="NavigationItems">
+        <NavigationItem link="/">Executive Careers</NavigationItem>
+        <NavigationItem link="/">The NCI Hiring Experience</NavigationItem>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default navigationItems;
