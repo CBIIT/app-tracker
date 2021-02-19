e7e60481e16d54bc5c11aa056eb8184f954bf45c
Larry Nguyen <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-02-19 14:35:51 -0500

    Fixed hover on dashboard.  PostCss.  Updated text.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "antd/dist/antd.css";
+import { Route, Switch } from 'react-router-dom';
+import 'antd/dist/antd.css';
 
-import "./App.css";
-import Layout from "./hoc/Layout/Layout";
-import Home from "./containers/Home/Home";
-import CreateVacancy from "./containers/CreateVacancy/CreateVacancy";
-import VacancyDashboard from "./containers/VacancyDashboard/VacancyDashboard";
+import './App.css';
+import Layout from './hoc/Layout/Layout';
+import Home from './containers/Home/Home';
+import CreateVacancy from './containers/CreateVacancy/CreateVacancy';
+import VacancyDashboard from './containers/VacancyDashboard/VacancyDashboard';
 
 function App() {
-  return (
-    <>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/create-vacancy" component={CreateVacancy} />
-          <Route path="/vacancy-dashboard" exact component={VacancyDashboard} />
-        </Switch>
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout>
+				<Switch>
+					<Route path='/' exact component={Home} />
+					<Route path='/create-vacancy' component={CreateVacancy} />
+					<Route path='/vacancy-dashboard' exact component={VacancyDashboard} />
+				</Switch>
+			</Layout>
+		</>
+	);
 }
 
 export default App;
--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,98 +1,98 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const servicenowConfig = require("./servicenow.config");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const servicenowConfig = require('./servicenow.config');
 
-const ROOT_PATH = path.join(__dirname, "../");
+const ROOT_PATH = path.join(__dirname, '../');
 
 const DEFAULTS = { ASSET_SIZE_LIMIT: 10000 };
 const CONFIG = { ...DEFAULTS, ...servicenowConfig };
 
 module.exports = {
-  entry: {
-    [CONFIG.JS_API_PATH + "app"]: ["./src/index.js"],
-  },
+	entry: {
+		[CONFIG.JS_API_PATH + 'app']: ['./src/index.js'],
+	},
 
-  output: {
-    publicPath: "/",
-    path: path.join(ROOT_PATH, "dist/"),
-  },
+	output: {
+		publicPath: '/',
+		path: path.join(ROOT_PATH, 'dist/'),
+	},
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+	},
 
-  rules: {
-    svg: {
-      test: /\.svg$/,
-      loader: "svg-url-loader",
-      options: {
-        noquotes: true,
-      },
-      exclude: /node_modules/,
-    },
+	rules: {
+		svg: {
+			test: /\.svg$/,
+			loader: 'svg-url-loader',
+			options: {
+				noquotes: true,
+			},
+			exclude: /node_modules/,
+		},
 
-    css: {
-      test: /\.(css)$/,
-      use: ["style-loader", "css-loader"],
-    },
+		css: {
+			test: /\.(css)$/,
+			use: ['style-loader', 'css-loader', 'postcss-loader'],
+		},
 
-    img: {
-      test: /\.(png|jpg|gif|)$/,
-      loader: "url-loader",
-      options: {
-        limit: CONFIG.ASSET_SIZE_LIMIT,
-        name: CONFIG.IMG_API_PATH + "[name]-[hash:6]-[ext]",
-      },
-    },
-    assets: {
-      test: /\.(woff|woff2|ttf|eot)$/,
-      loader: "url-loader",
-      options: {
-        limit: CONFIG.ASSET_SIZE_LIMIT,
-        name: CONFIG.ASSETS_API_PATH + "[name]-[hash:6]-[ext]",
-      },
-    },
+		img: {
+			test: /\.(png|jpg|gif|)$/,
+			loader: 'url-loader',
+			options: {
+				limit: CONFIG.ASSET_SIZE_LIMIT,
+				name: CONFIG.IMG_API_PATH + '[name]-[hash:6]-[ext]',
+			},
+		},
+		assets: {
+			test: /\.(woff|woff2|ttf|eot)$/,
+			loader: 'url-loader',
+			options: {
+				limit: CONFIG.ASSET_SIZE_LIMIT,
+				name: CONFIG.ASSETS_API_PATH + '[name]-[hash:6]-[ext]',
+			},
+		},
 
-    jsx(args) {
-      args = args || {};
-      var emitWarning = false;
-      if (args.withHot) {
-        // report eslint errors as warnings so hot reloads are not prevented
-        emitWarning = true;
-      }
+		jsx(args) {
+			args = args || {};
+			var emitWarning = false;
+			if (args.withHot) {
+				// report eslint errors as warnings so hot reloads are not prevented
+				emitWarning = true;
+			}
 
-      return {
-        include: [path.join(ROOT_PATH, "src")],
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-          {
-            loader: "eslint-loader",
-            options: {
-              emitWarning,
-            },
-          },
-        ],
-      };
-    },
-  },
+			return {
+				include: [path.join(ROOT_PATH, 'src')],
+				test: /\.(ts|tsx|js|jsx)$/,
+				exclude: /(node_modules)/,
+				use: [
+					{
+						loader: 'babel-loader',
+					},
+					{
+						loader: 'eslint-loader',
+						options: {
+							emitWarning,
+						},
+					},
+				],
+			};
+		},
+	},
 
-  plugins: {
-    createIndexHtml() {
-      return createHtmlPluginInstance({
-        filename: "index.html",
-        title: "",
-        chunks: "app",
-        template: "src/index.html",
-      });
-    },
-  },
+	plugins: {
+		createIndexHtml() {
+			return createHtmlPluginInstance({
+				filename: 'index.html',
+				title: '',
+				chunks: 'app',
+				template: 'src/index.html',
+			});
+		},
+	},
 };
 
 function createHtmlPluginInstance(cfg) {
-  cfg.inject = true;
-  return new HtmlWebPackPlugin(cfg);
+	cfg.inject = true;
+	return new HtmlWebPackPlugin(cfg);
 }
