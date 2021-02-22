c16ef586cf386f18fa5be6ec0f20fb9985fea528
Nick Ruggieri <ruggierine@nih.gov>
2021-02-22 17:23:29 -0500

    adding login files

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "antd";
-import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+// import { Authenticate } from '../containers/Login/Login.js';
 
-import "./Login.css";
+import './Login.css';
 
 const login = () => (
-  <Link to="/vacancy-dashboard">
-    <div className="Login">
-      {/* <Dropdown overlay={menu}> */}
+	<Link to='/vacancy-dashboard'>
+		<div className='Login'>
+			{/* <Dropdown overlay={menu}> */}
 
-      <Button type="primary" ghost>
-        <UserOutlined /> Login <DownOutlined />
-      </Button>
-      {/* </Dropdown> */}
-    </div>
-  </Link>
+			<Button type='primary' ghost>
+				<UserOutlined /> Login <DownOutlined />
+			</Button>
+			{/* </Dropdown> */}
+		</div>
+	</Link>
 );
 
 export default login;
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,11 @@
+function authenticate() {
+	// $location.url(
+	// 	'nav_to.do?uri=' +
+	// 		encodeURIComponent($location.url()) +
+	// 		'&glide_sso_id=' +
+	// 		c.data.defaultIDP
+	// );
+	console.log('I was clicked');
+}
+
+export default authenticate;
