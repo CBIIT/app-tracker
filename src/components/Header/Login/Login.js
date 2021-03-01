7a3aa9da09a84db2cf29016c1d5b0a4aedfc1234
Nick Ruggieri <ruggierine@nih.gov>
2021-03-01 13:12:58 -0500

    adding login calls

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import { Redirect, Route } from 'react-router-dom';
 // import { Authenticate } from '../containers/Login/Login.js';
 
 import './Login.css';
@@ -17,11 +17,35 @@ const login = () => (
 	</div>
 );
 
-const authenticate = () => {
-	location.href(
-		'nav_to.do?uri=' +
-			encodeURIComponent(location.href + '&glide_sso_id=' + c.data.defaultIDP)
+const authenticate = async () => {
+	let res = await fetch('/api/x_g_nci_app_tracke/login/check_auth').then(
+		(res) => {
+			return res.json();
+		}
 	);
+
+	<>
+		<Route>
+			{res.result.logged_in == true ? (
+				<Redirect to='/vacancy-dashboard' />
+			) : (
+				(location.href =
+					'nav_to.do?uri=' +
+					'nci-vms.do#/' +
+					'?TARGET=https://service-dev2.nci.nih.gov/nci-vms.do#/vacancy-dashboard')
+			)}
+		</Route>
+
+		{/* if (res.result.logged_in == true){
+
+  }
+  else{
+    location.href = 
+      'nav_to.do?uri=' +
+        encodeURIComponent(location.href + '&glide_sso_id=' + res.result.default_idp)
+
+  } */}
+	</>;
 };
 
 export default login;
--- webpack/servicenow.config.js
+++ webpack/servicenow.config.js
@@ -1,47 +1,47 @@
 const servicenowConfig = {
-  /**
-   * This is a default prefix for all ServiceNow APIs
-   * should not be changed
-   */
-  REST_API_PATH: '/api',
-  /**
-   * ServiceNow instance URL for REST calls
-   * it is being used in DEVELOPMENT mode only
-   * This should be the instance where React application will be deployed to
-   */
-  SERVICENOW_INSTANCE: '',
-  /**
-   * User Name for API requests
-   * it is being used for sending REST calls in DEVELOPMENT mode only
-   * no need to provide credentials for PRODUCTION
-   */
-  REACT_APP_USER: '',
-  /**
-   * User password, for DEVELOPMENT mode only
-   */
-  REACT_APP_PASSWORD: '',
-  /**
-   * ServiceNow path to GET resource which serves javascript files
-   * Current configuration does not produce CSS files
-   * CSS code will be embedded into javascript files
-   */
-  JS_API_PATH: 'api/x_g_nci_app_tracke/container/js/',
-  /**
-   * ServiceNow path to GET resource which serves
-   * Image files (png, jpg, gif)
-   * SVG files will be embedded into javascript files
-   */
-  IMG_API_PATH: 'api/x_g_nci_app_tracke/container/img/',
-  /**
-   * ServiceNow path to GET resource which serves
-   * other files, like fonts etc.
-   */
-  ASSETS_API_PATH: 'api/x_g_nci_app_tracke/container/other_assets/',
-  /**
-   * fonts and images below this size will be put inside
-   * JS chunks, instead of being saved as separate files
-   */
-  ASSET_SIZE_LIMIT: 10000,
-}
+	/**
+	 * This is a default prefix for all ServiceNow APIs
+	 * should not be changed
+	 */
+	REST_API_PATH: '/api',
+	/**
+	 * ServiceNow instance URL for REST calls
+	 * it is being used in DEVELOPMENT mode only
+	 * This should be the instance where React application will be deployed to
+	 */
+	SERVICENOW_INSTANCE: 'https://service-dev2.nci.nih.gov',
+	/**
+	 * User Name for API requests
+	 * it is being used for sending REST calls in DEVELOPMENT mode only
+	 * no need to provide credentials for PRODUCTION
+	 */
+	REACT_APP_USER: '',
+	/**
+	 * User password, for DEVELOPMENT mode only
+	 */
+	REACT_APP_PASSWORD: '',
+	/**
+	 * ServiceNow path to GET resource which serves javascript files
+	 * Current configuration does not produce CSS files
+	 * CSS code will be embedded into javascript files
+	 */
+	JS_API_PATH: 'api/x_g_nci_app_tracke/container/js/',
+	/**
+	 * ServiceNow path to GET resource which serves
+	 * Image files (png, jpg, gif)
+	 * SVG files will be embedded into javascript files
+	 */
+	IMG_API_PATH: 'api/x_g_nci_app_tracke/container/img/',
+	/**
+	 * ServiceNow path to GET resource which serves
+	 * other files, like fonts etc.
+	 */
+	ASSETS_API_PATH: 'api/x_g_nci_app_tracke/container/other_assets/',
+	/**
+	 * fonts and images below this size will be put inside
+	 * JS chunks, instead of being saved as separate files
+	 */
+	ASSET_SIZE_LIMIT: 10000,
+};
 
-module.exports = servicenowConfig
+module.exports = servicenowConfig;
