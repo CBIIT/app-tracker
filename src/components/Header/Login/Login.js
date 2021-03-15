6ff128e98b697639dff406e4d2e67c60dee33a26
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-03-15 16:00:13 -0400

    Login sandbox code.

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,21 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "antd";
-import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Button, Menu, Dropdown } from 'antd';
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
 
-import "./Login.css";
+import './Login.css';
 
-const login = () => (
-  <Link to="/vacancy-dashboard">
-    <div className="Login">
-      {/* <Dropdown overlay={menu}> */}
+const okta = () => {
+	location.href =
+		'nav_to.do?uri=' +
+		encodeURIComponent('/nci-vms.do#/vacancy-dashboard') +
+		'&glide_sso_id=7fa8fb711b6e6050e541631ee54bcb69';
+};
 
-      <Button type="primary" ghost>
-        <UserOutlined /> Login <DownOutlined />
-      </Button>
-      {/* </Dropdown> */}
-    </div>
-  </Link>
-);
+const login = () => {
+	const history = useHistory();
+
+	const handleMenuClick = (e) => {
+		switch (e.key) {
+			case 'itrust':
+				location.href =
+					'nav_to.do?uri=' +
+					encodeURIComponent('/nci-vms.do#/vacancy-dashboard') +
+					'&glide_sso_id=14a8eb8cdb5b320054d8ff621f9619d3';
+				break;
+			case 'okta':
+				console.log('[Login]: ' + window.location);
+				history.push(
+					'nav_to.do?uri=' +
+						encodeURIComponent('/nci-vms.do#/vacancy-dashboard') +
+						'&glide_sso_id=7fa8fb711b6e6050e541631ee54bcb69'
+				);
+				break;
+			default:
+				return;
+		}
+	};
+
+	const menu = (
+		<Menu onClick={handleMenuClick}>
+			<Menu.Item key='itrust' icon={<UserOutlined />}>
+				iTrust
+			</Menu.Item>
+			<Menu.Item key='okta' icon={<UserOutlined />}>
+				okta
+			</Menu.Item>
+		</Menu>
+	);
+
+	// <Link to='/vacancy-dashboard'>
+	return (
+		<div className='Login'>
+			<Dropdown overlay={menu}>
+				<Button type='primary' ghost>
+					<UserOutlined /> Login <DownOutlined />
+				</Button>
+			</Dropdown>
+		</div>
+	);
+	// </Link>
+};
 
 export default login;
