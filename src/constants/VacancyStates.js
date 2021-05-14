5f0f13d1c96d79077cab82bdf74b76c630343841
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-05-14 11:15:58 -0400

    Advance to committee scoring button.  Rating plan.

--- src/constants/VacancyStates.js
+++ src/constants/VacancyStates.js
@@ -1,4 +1,5 @@
 const OWM_TRIAGE = 'owm_triage';
 const CHAIR_TRIAGE = 'chair_triage';
+const INDIVIDUAL_SCORING_IN_PROGRESS = 'individual_scoring_in_progress';
 
-export { OWM_TRIAGE, CHAIR_TRIAGE };
+export { OWM_TRIAGE, CHAIR_TRIAGE, INDIVIDUAL_SCORING_IN_PROGRESS };
