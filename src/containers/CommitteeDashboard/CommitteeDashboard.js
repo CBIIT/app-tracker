6dde5821e47696283aab65c6d7fdfee4a687baef
Nick Ruggieri <ruggierine@nih.gov>
2021-05-20 16:29:07 -0400

    Adding correct data entry to committee table

--- src/containers/CommitteeDashboard/CommitteeDashboard.js
+++ src/containers/CommitteeDashboard/CommitteeDashboard.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { MANAGE_VACANCY } from '../../constants/Routes.js';
 import { GET_COMMITTEE_MEMBER_VIEW } from '../../constants/ApiEndpoints';
 import { Table } from 'antd';
-// import './CommitteeDashboard.css';
 import axios from 'axios';
 
 const renderDecision = (text) =>
@@ -23,8 +22,6 @@ const committeeDashboard = () => {
 		(async () => {
 			try {
 				const currentData = await axios.get(GET_COMMITTEE_MEMBER_VIEW);
-				debugger;
-				console.log(currentData);
 				setData(currentData.data.result);
 			} catch (err) {
 				console.warn(err);
@@ -58,22 +55,30 @@ const committeeColumns = [
 		title: 'Vacancy Title',
 		dataIndex: 'vacancy_title',
 		key: 'title',
-		// sorter: {
-		// 	compare: (a, b) => a.vacancy_title.localeCompare(b.vacancy_title),
-		// 	multiple: 1,
-		// },
+		sorter: {
+			compare: (a, b) => a.vacancy_title.localeCompare(b.vacancy_title),
+			multiple: 1,
+		},
 		defaultSortOrder: 'ascend',
-		// render: (title, record) => (
-		//  <Link to={'/manage/vacancy/' + record.vacancy_id}>{title}</Link>
-		// ),
+		render: (title, record) => (
+			<Link to={'/manage/vacancy/' + record.vacancy_id}>{title}</Link>
+		),
 	},
 	{
 		title: 'Applicants',
 		dataIndex: 'applicants',
 		key: 'applicants',
 		render: (number, record) => (
-			<Link to={MANAGE_VACANCY + record.vacancy_id + '/applicants'}>
-				{number} {number == 1 ? 'applicant' : 'applicants'}
+			<Link
+				key={record.vacancy_id}
+				to={MANAGE_VACANCY + record.vacancy_id + '/applicants'}
+			>
+				{number}{' '}
+				{number == 1
+					? 'applicant'
+					: number == undefined
+					? '0 applicants'
+					: 'applicants'}
 			</Link>
 		),
 	},
@@ -81,31 +86,29 @@ const committeeColumns = [
 		title: 'Status',
 		dataIndex: 'status',
 		key: 'status',
-		// sorter: {
-		// 	compare: (a, b) => a.status.localeCompare(b.status),
-		// 	multiple: 2,
-		// },
+		sorter: {
+			compare: (a, b) => a.status.localeCompare(b.status),
+			multiple: 2,
+		},
 		defaultSortOrder: 'ascend',
-		// render: (status) => {
-		// 	// 	if (status.includes('owm')) {
-		// 	// 		status = status.split('_')[0].toUpperCase() + status.substring(3);
-		// 	// 	}
-		// 	if (status.includes('_')) {
-		// 		status = status
-		// 			.split('_')
-		// 			.map((word) => word[0].toUpperCase() + word.substring(1))
-		// 			.join(' ');
-		// 		return <span style={{ color: 'rgb(86,86,86)' }}>{status}</span>;
-		// 	}
-		// },
-		// else {
-		// 		return (
-		// 			<span style={{ color: 'rgb(86,86,86)' }}>
-		// 				{status.charAt(0).toUpperCase() + status.slice(1)}
-		// 			</span>
-		// 		);
-		// 	}
-		// },
+		render: (status) => {
+			if (status.includes('owm')) {
+				status = status.split('_')[0].toUpperCase() + status.substring(3);
+			}
+			if (status.includes('_')) {
+				status = status
+					.split('_')
+					.map((word) => word[0].toUpperCase() + word.substring(1))
+					.join(' ');
+				return <span style={{ color: 'rgb(86,86,86)' }}>{status}</span>;
+			} else {
+				return (
+					<span style={{ color: 'rgb(86,86,86)' }}>
+						{status.charAt(0).toUpperCase() + status.slice(1)}
+					</span>
+				);
+			}
+		},
 	},
 	{
 		title: 'Your Scoring',
