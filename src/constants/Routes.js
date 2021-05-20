5b7fed39a1f7abb1ecdce842dcd7b74e90e30f27
Nick Ruggieri <ruggierine@nih.gov>
2021-05-20 14:46:51 -0400

    adding route export

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,4 +1,4 @@
 const MANAGE_APPLICATION = '/manage/application/';
 const MANAGE_VACANCY = '/manage/vacancy/';
 const COMMITTEE_DASHBOARD = '/committee-dashboard/';
-export { MANAGE_APPLICATION, MANAGE_VACANCY };
+export { MANAGE_APPLICATION, MANAGE_VACANCY, COMMITTEE_DASHBOARD };
