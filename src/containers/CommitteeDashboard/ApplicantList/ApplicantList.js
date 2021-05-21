b5132408177404e15cbfe88ae4130fc9cbbd36b3
Nick Ruggieri <ruggierine@nih.gov>
2021-05-21 16:45:11 -0400

    Adding committee member vacancy list

--- src/containers/CommitteeDashboard/ApplicantList/ApplicantList.js
+++ src/containers/CommitteeDashboard/ApplicantList/ApplicantList.js
@@ -1,17 +1,16 @@
 import { Table } from 'antd';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-// import { MANAGE_APPLICATION } from '../../../constants/Routes';
-// import './ApplicantList.css';
+import { MANAGE_APPLICATION } from '../../../constants/Routes';
 
-// const renderDecision = (text) =>
-// 	text == 'Pending' ? (
-// 		<span style={{ color: 'rgba(0,0,0,0.25)', textTransform: 'capitalize' }}>
-// 			{text}
-// 		</span>
-// 	) : (
-// 		<span style={{ textTransform: 'capitalize' }}>{text}</span>
-// 	);
+const renderDecision = (text) =>
+	text == 'Pending' ? (
+		<span style={{ color: 'rgba(0,0,0,0.25)', textTransform: 'capitalize' }}>
+			{text}
+		</span>
+	) : (
+		<span style={{ textTransform: 'capitalize' }}>{text}</span>
+	);
 
 const applicantList = (props) => {
 	const applicants = props.applicants;
@@ -24,13 +23,13 @@ const applicantList = (props) => {
 			title: 'Applicant',
 			dataIndex: 'applicant_last_name',
 			key: 'name',
-			// render: (text, record) => {
-			// 	return (
-			// 		<Link to={MANAGE_APPLICATION + record.sys_id}>
-			// 			{text}, {record.applicant_first_name}
-			// 		</Link>
-			// 	);
-			// },
+			render: (text, record) => {
+				return (
+					<Link to={MANAGE_APPLICATION + record.sys_id}>
+						{text}, {record.applicant_first_name}
+					</Link>
+				);
+			},
 			sorter: {
 				compare: (a, b) =>
 					a.applicant_last_name.localeCompare(b.applicant_last_name),
@@ -48,23 +47,29 @@ const applicantList = (props) => {
 			title: 'Raw Score',
 			dataIndex: 'raw_score',
 			key: 'rawscore',
-			// render: (text) => {
-			// 	return <span>{text.split(' ')[0]}</span>;
-			// },
+			render: (text) => {
+				return <span>{text.split(' ')[0]}</span>;
+			},
 		},
 
 		{
 			title: 'Average Score',
 			dataIndex: 'average_score',
 			key: 'averagescore',
-			// render: (text) => renderDecision(text),
+			render: (text) => renderDecision(text),
+		},
+		{
+			title: 'Recommend Interview?',
+			dataIndex: 'recommend',
+			key: 'recommend',
+			render: (text) => renderDecision(text),
 		},
 	];
 
 	return (
 		<div className='applicant-table'>
 			<Table
-				// dataSource={applicants}
+				dataSource={applicants}
 				columns={applicantColumns}
 				key='applicants'
 			></Table>
