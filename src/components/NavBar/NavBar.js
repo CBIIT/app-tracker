6972dbb7d61c464af5797dd908eb4d8679dc7894
Nick Ruggieri <ruggierine@nih.gov>
2021-06-10 19:31:40 -0400

    Adding navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,59 @@
 import { Menu } from 'antd';
+import { CHECK_AUTH } from '../../constants/ApiEndpoints';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './NavBar.css';
 
-const NavBar = () => {
-	return (
-		<>
-			<div className='NavBar'>
-				<Menu mode='horizontal'>
-					<Menu.Item>
-						<Link to='/'>Home</Link>
-					</Menu.Item>
-					<Menu.Item>
-						<Link to='/vacancy-dashboard'>Vacancy Dashboard</Link>
-					</Menu.Item>
-				</Menu>
-			</div>
-		</>
-	);
+const navBar = () => {
+	const [authResponse, setAuthResponse] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const currentAuthResponse = await axios.get(CHECK_AUTH);
+				console.log('[NAV AUTH RESPONSE]:', currentAuthResponse);
+				setAuthResponse(currentAuthResponse.data.result);
+				// debugger;
+			} catch (err) {
+				console.warn(err);
+			}
+		})();
+	}, []);
+
+	// debugger;
+
+	if (authResponse.logged_in == true) {
+		return (
+			<>
+				<div className='NavBar'>
+					<Menu mode='horizontal'>
+						<Menu.Item key='home'>
+							<Link to='/'>Home</Link>
+						</Menu.Item>
+						<Menu.Item key='dashboard'>
+							<Link to='/vacancy-dashboard'>Vacancies Dashboard</Link>
+						</Menu.Item>
+					</Menu>
+				</div>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<div className='NavBar'>
+					<Menu mode='horizontal'>
+						<Menu.Item key='home'>
+							<Link to='/'>Home</Link>
+						</Menu.Item>
+						<Menu.Item key='hiring'>
+							<Link to='/'>The NCI Hiring Experience</Link>
+						</Menu.Item>
+					</Menu>
+				</div>
+			</>
+		);
+	}
 };
 
-export default NavBar;
+export default navBar;
