6d205df6ea901afe9674b8512e9746b0e0b8516f
Nick Ruggieri <ruggierine@nih.gov>
2021-06-21 16:30:57 -0400

    Adding outer div to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -23,48 +23,54 @@ const navBar = () => {
 		if (authResponse.is_manager == true && authResponse.is_exec_sec == true) {
 			return (
 				<>
-					<div className='NavBar'>
-						<Menu mode='horizontal'>
-							<Menu.Item key='home'>
-								<Link to='/'>Home</Link>
-							</Menu.Item>
-							<Menu.Item key='vacancy-dashboard'>
-								<Link to='/vacancy-dashboard'>Vacancy Dashboard</Link>
-							</Menu.Item>
-							<Menu.Item key='your-vacancies'>
-								<Link to='/committee-dashboard'>Your Vacancies</Link>
-							</Menu.Item>
-						</Menu>
+					<div className='OuterDiv'>
+						<div className='NavBar'>
+							<Menu mode='horizontal'>
+								<Menu.Item key='home'>
+									<Link to='/'>Home</Link>
+								</Menu.Item>
+								<Menu.Item key='vacancy-dashboard'>
+									<Link to='/vacancy-dashboard'>Vacancy Dashboard</Link>
+								</Menu.Item>
+								<Menu.Item key='your-vacancies'>
+									<Link to='/committee-dashboard'>Your Vacancies</Link>
+								</Menu.Item>
+							</Menu>
+						</div>
 					</div>
 				</>
 			);
 		} else if (authResponse.is_manager == true) {
 			return (
 				<>
-					<div className='NavBar'>
-						<Menu mode='horizontal'>
-							<Menu.Item key='home'>
-								<Link to='/'>Home</Link>
-							</Menu.Item>
-							<Menu.Item key='vacancy-dashboard'>
-								<Link to='/vacancy-dashboard'>Vacancy Dashboard</Link>
-							</Menu.Item>
-						</Menu>
+					<div className='OuterDiv'>
+						<div className='NavBar'>
+							<Menu mode='horizontal'>
+								<Menu.Item key='home'>
+									<Link to='/'>Home</Link>
+								</Menu.Item>
+								<Menu.Item key='vacancy-dashboard'>
+									<Link to='/vacancy-dashboard'>Vacancy Dashboard</Link>
+								</Menu.Item>
+							</Menu>
+						</div>
 					</div>
 				</>
 			);
 		} else if (authResponse.is_chair == true) {
 			return (
 				<>
-					<div className='NavBar'>
-						<Menu mode='horizontal'>
-							<Menu.Item key='home'>
-								<Link to='/'>Home</Link>
-							</Menu.Item>
-							<Menu.Item key='your-vacancies'>
-								<Link to='/chair-dashboard'>Your Vacancies</Link>
-							</Menu.Item>
-						</Menu>
+					<div className='OuterDiv'>
+						<div className='NavBar'>
+							<Menu mode='horizontal'>
+								<Menu.Item key='home'>
+									<Link to='/'>Home</Link>
+								</Menu.Item>
+								<Menu.Item key='your-vacancies'>
+									<Link to='/chair-dashboard'>Your Vacancies</Link>
+								</Menu.Item>
+							</Menu>
+						</div>
 					</div>
 				</>
 			);
@@ -74,30 +80,34 @@ const navBar = () => {
 		) {
 			return (
 				<>
-					<div className='NavBar'>
-						<Menu mode='horizontal'>
-							<Menu.Item key='home'>
-								<Link to='/'>Home</Link>
-							</Menu.Item>
-							<Menu.Item key='your-vacancies'>
-								<Link to='/committee-dashboard'>Your Vacancies</Link>
-							</Menu.Item>
-						</Menu>
+					<div className='OuterDiv'>
+						<div className='NavBar'>
+							<Menu mode='horizontal'>
+								<Menu.Item key='home'>
+									<Link to='/'>Home</Link>
+								</Menu.Item>
+								<Menu.Item key='your-vacancies'>
+									<Link to='/committee-dashboard'>Your Vacancies</Link>
+								</Menu.Item>
+							</Menu>
+						</div>
 					</div>
 				</>
 			);
 		} else {
 			return (
 				<>
-					<div className='NavBar'>
-						<Menu mode='horizontal'>
-							<Menu.Item key='home'>
-								<Link to='/'>Home</Link>
-							</Menu.Item>
-							<Menu.Item key='your-applications'>
-								<Link to='/applicant-dashboard'>Your Applications</Link>
-							</Menu.Item>
-						</Menu>
+					<div className='OuterDiv'>
+						<div className='NavBar'>
+							<Menu mode='horizontal'>
+								<Menu.Item key='home'>
+									<Link to='/'>Home</Link>
+								</Menu.Item>
+								<Menu.Item key='your-applications'>
+									<Link to='/applicant-dashboard'>Your Applications</Link>
+								</Menu.Item>
+							</Menu>
+						</div>
 					</div>
 				</>
 			);
@@ -105,17 +115,25 @@ const navBar = () => {
 	} else {
 		return (
 			<>
-				<div className='NavBar'>
-					<Menu mode='horizontal'>
-						<Menu.Item key='home'>
-							<Link to='/'>Home</Link>
-						</Menu.Item>
-						<Menu.Item key='hiring'>
-							<Link to='https://hr.nih.gov/jobs/executive/recruit/nih-executive-experience'>
+				<div className='OuterDiv'>
+					<div className='NavBar'>
+						<Menu mode='horizontal'>
+							<Menu.Item key='home'>
+								<Link to='/'>Home</Link>
+							</Menu.Item>
+							<Menu.Item
+								key='hiring'
+								onClick={() =>
+									window.open(
+										'https://hr.nih.gov/jobs/executive/recruit/nih-executive-experience',
+										'_blank'
+									)
+								}
+							>
 								The NCI Hiring Experience
-							</Link>
-						</Menu.Item>
-					</Menu>
+							</Menu.Item>
+						</Menu>
+					</div>
 				</div>
 			</>
 		);
