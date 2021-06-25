05b832ffcbf89af64ca9cd5529a2ddaca50b982e
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-06-25 16:10:06 -0400

    Hide next stage button on certain stages.

--- src/constants/VacancyStates.js
+++ src/constants/VacancyStates.js
@@ -4,6 +4,8 @@ const INDIVIDUAL_SCORING_IN_PROGRESS = 'individual_scoring_in_progress';
 const COMMITTEE_REVIEW_IN_PROGRESS = 'committee_review_in_progress';
 const COMMITTEE_REVIEW_COMPLETE = 'committee_review_complete';
 const VOTING_COMPLETE = 'voting_complete';
+const LIVE = 'live';
+const FINAL = 'final';
 export {
 	OWM_TRIAGE,
 	CHAIR_TRIAGE,
@@ -11,4 +13,6 @@ export {
 	COMMITTEE_REVIEW_IN_PROGRESS,
 	VOTING_COMPLETE,
 	COMMITTEE_REVIEW_COMPLETE,
+	LIVE,
+	FINAL,
 };
