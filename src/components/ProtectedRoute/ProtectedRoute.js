60f4a310842ba037562c60e85a4c4b10a6d8f713
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-06-29 16:08:00 -0400

    ProtectedRoute component.

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,11 @@
-const protectedRoute = ({
-	component: Component,
-	isUserLoggedIn,
-	...rest
-}) => {};
+import { Route, Redirect } from 'react-router-dom';
+
+const protectedRoute = ({ component: Component, isUserLoggedIn, ...rest }) => {
+	isUserLoggedIn ? (
+		<Route {...rest} render={(props) => <Component {...rest} {...props} />} />
+	) : (
+		<Redirect />
+	);
+};
 
 export default protectedRoute;
