353fed7d60f413d306647476bae6e9301ead85d7
Nick Ruggieri <ruggierine@nih.gov>
2021-07-06 17:28:45 -0400

    Adding change to date

--- src/containers/VacancyDashboard/VacancyDashboard.js
+++ src/containers/VacancyDashboard/VacancyDashboard.js
@@ -218,9 +218,7 @@ const vacancyDashboard = () => {
 		{
 			title: 'Open Date',
 			dataIndex: 'open_date',
-			render: (date) => {
-				transformDateToDisplay(date);
-			},
+			render: (date) => transformDateToDisplay(date),
 			sorter: {
 				compare: (a, b) => {
 					const dateA = a.open_date;
