79a2d958a823076dccfe4a2b19e6142e0427d0a3
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2021-11-23 13:31:50 -0500

    Updated support email.

--- src/components/Footer/ExternalNav/ExternalNav.js
+++ src/components/Footer/ExternalNav/ExternalNav.js
@@ -3,32 +3,53 @@ import React from 'react';
 import './ExternalNav.css';
 
 const externalNav = () => (
-    <div className='ExternalNav'>
-        <div className='ExternalNavContent'>
-            <nav>
-                <ul>
-                    <li><a href='https://ccr.cancer.gov/'>Home</a></li>
-                    <li><a href='mailto:CCRCareersAdmin@mail.nih.gov'>Support</a></li>
-                    <li><a href='https://www.cancer.gov/global/web/policies'>Policies</a></li>
-                    <li><a href='https://www.cancer.gov/global/web/policies/accessibility'>Accessibility</a></li>
-                    <li><a href='https://cancer.gov/global/viewing-files'>Viewing Files</a></li>
-                    <li><a href='https://www.cancer.gov/global/web/policies/foia'>FOIA</a></li>
-                </ul>
-                <ul>
-                    <li><a href='https://www.hhs.gov/'>U.S Department of Health and Human Services</a></li>
-                    <li><a href='https://www.nih.gov/'>National Institutes of Health</a></li>
-                    <li><a href='https://www.cancer.gov/'>National Cancer Institute</a></li>
-                    <li><a href='https://usa.gov/'>USA.gov</a></li>
-                </ul>
-            </nav>
-        </div>
-        <div className='ExternalNavTrademark'>NIH ... Turning Discovery Into Health ®</div>
-    </div>
+	<div className='ExternalNav'>
+		<div className='ExternalNavContent'>
+			<nav>
+				<ul>
+					<li>
+						<a href='https://ccr.cancer.gov/'>Home</a>
+					</li>
+					<li>
+						<a href='mailto:nciowmstaff@mail.nih.gov'>Support</a>
+					</li>
+					<li>
+						<a href='https://www.cancer.gov/global/web/policies'>Policies</a>
+					</li>
+					<li>
+						<a href='https://www.cancer.gov/global/web/policies/accessibility'>
+							Accessibility
+						</a>
+					</li>
+					<li>
+						<a href='https://cancer.gov/global/viewing-files'>Viewing Files</a>
+					</li>
+					<li>
+						<a href='https://www.cancer.gov/global/web/policies/foia'>FOIA</a>
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<a href='https://www.hhs.gov/'>
+							U.S Department of Health and Human Services
+						</a>
+					</li>
+					<li>
+						<a href='https://www.nih.gov/'>National Institutes of Health</a>
+					</li>
+					<li>
+						<a href='https://www.cancer.gov/'>National Cancer Institute</a>
+					</li>
+					<li>
+						<a href='https://usa.gov/'>USA.gov</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div className='ExternalNavTrademark'>
+			NIH ... Turning Discovery Into Health ®
+		</div>
+	</div>
 );
 
 export default externalNav;
-
-
-// Home | Support | Policies | Accessibility | Viewing Files | FOIA
-// U.S. Department of Health and Human Services | National Institutes of Health | National Cancer Institute | USA.gov
-// NIH ... Turning Discovery Into Health ®
