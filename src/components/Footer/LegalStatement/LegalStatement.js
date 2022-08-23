2705bd575ae3755abb1d526e1c5ca1a6037bb1c1
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2022-08-23 10:42:40 -0400

    Updated second paragraph of OMB statement.

--- src/components/Footer/LegalStatement/LegalStatement.js
+++ src/components/Footer/LegalStatement/LegalStatement.js
@@ -23,20 +23,17 @@ const legalStatement = () => (
 			</p>
 			<p>
 				Public reporting burden for this collection of information is estimated
-				to average 30 minutes per response, including the time for reviewing
+				to average two minutes per response, including the time for reviewing
 				instructions, searching existing data sources, gathering and maintaining
 				the data needed, and completing and reviewing the collection of
-				information.{' '}
-				<strong>
-					An agency may not conduct or sponsor, and a person is not required to
-					respond to, a collection of information unless it displays a currently
-					valid OMB control number.
-				</strong>{' '}
-				Send comments regarding this burden estimate or any other aspect of this
-				collection of information, including suggestions for reducing this
-				burden to: NIH, Project Clearance Branch, 6705 Rockledge Drive, MSC
-				7974, Bethesda, MD 20892-7974, ATTN: PRA (0925-0761). Do not return the
-				completed form to this address.
+				information. An agency may not conduct or sponsor, and a person is not
+				required to respond to, a collection of information unless it displays a
+				currently valid OMB control number. Send comments regarding this burden
+				estimate or any other aspect of this collection of information,
+				including suggestions for reducing this burden to: NIH, Project
+				Clearance Branch, 6705 Rockledge Drive, MSC 7974, Bethesda, MD
+				20892-7974, ATTN: PRA (0925-0046). Do not return the completed form to
+				this address
 			</p>
 		</div>
 	</div>
