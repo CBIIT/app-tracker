93991be947d2a42100fbddc6a1bfc6b53aadb0a7
larry-nguyen-nih <73351406+larry-nguyen-nih@users.noreply.github.com>
2022-09-16 16:05:46 -0400

    New landing page image that is smaller size.

--- webpack/add-extensions.sh
+++ webpack/add-extensions.sh
@@ -1,1 +1,1 @@
-zmv 'dist/api/x_g_nci_app_tracke/container/img/*' '$f.png' && zmv 'dist/api/x_g_nci_app_tracke/container/js/*' '$f.js'
+zmv 'dist/api/x_g_nci_app_tracke/container/img/(*)-(png|jpg)' '$f.$2' && zmv 'dist/api/x_g_nci_app_tracke/container/js/*' '$f.js'
