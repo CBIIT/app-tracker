b15ed857cae72873535ae5d270787db075e012f7
Harvey <harveybr@nci-02277589-ml.lan>
2023-04-24 12:46:40 -0500

    components refactored to tabs on applicantcard

--- src/containers/Profile/ApplicantCard/ApplicantCard.js
+++ src/containers/Profile/ApplicantCard/ApplicantCard.js
@@ -1,36 +1,29 @@
-import { useEffect, useState, useContext } from 'react';
-import { Avatar, Card, message, Typography, Divider } from 'antd';
+import { useState, useContext } from 'react';
+import { Avatar, Card, Typography, Divider } from 'antd';
 const { Paragraph, Title } = Typography;
 
 import ProfileContext from '../Util/FormContext';
-import DemographicsForm from '../Forms/Demographics';
+import DemographicTab from './Tabs/DemographicTab';
 import BasicInfoTab from './Tabs/BasicInfoTab';
-//import EditableBasicInfo from '../Forms/EditableBasicInfo/EditableBasicInfo';
+
+const tabList = [
+	{
+		key: 'basicInfo',
+		tab: 'Basic Information'
+	},
+	{
+		key: 'demographics',
+		tab: 'Demographics'
+	}
+];
+
+const contentObject = {
+	basicInfo: <BasicInfoTab />,
+	demographics: <DemographicTab />
+}
 
 const ApplicantCard = () => {
-	const [demoOpen, setDemoOpen] = useState(false);
-	const [basicOpen, setBasicOpen] = useState(false);
-
-	const contextValue = useContext(ProfileContext);
-	const { profile } = contextValue;
-
-	const getEthnicity = (value) => {
-		switch (value) {
-			case '1':
-				return 'Hispanic or Latino';
-			case '0':
-				return 'Not Hispanic or Latino';
-			default:
-				return '';
-		}
-	};
-
-	const getFullNumber = (prefix, number) => {
-		const areaCode = number.slice(0, 3);
-		const firstHalf = number.slice(3, 6);
-		const secondHalf = number.slice(6);
-		return `${prefix} (${areaCode}) ${firstHalf} - ${secondHalf}`;
-	};
+	const [activeTab, setActiveTab] = useState('basicInfo');
 
 	const getFirstInitial = (first) => {
 		const firstName = first.split('');
@@ -42,12 +35,21 @@ const ApplicantCard = () => {
 		return lastName[0];
 	};
 
-	const { basicInfo, demographics } = profile;
-	const address = basicInfo?.address;
+	const tabChange = (key) => {
+		setActiveTab(key);
+	};
+
+	const contextValue = useContext(ProfileContext);
+	const { profile } = contextValue;
+
+	const { basicInfo } = profile;
 
 	return (
 		<Card
 			style={{ width: '100%', height: '100%' }}
+			tabList={tabList}
+			activeTabKey={activeTab}
+			onTabChange={tabChange}
 			title={
 				<div
 					style={{
@@ -77,127 +79,7 @@ const ApplicantCard = () => {
 				</div>
 			}
 		>
-			<div style={{ marginLeft: '60px', marginRight: '60px' }}>
-				<BasicInfoTab/>
-				{/* {basicOpen ? (
-					<EditableBasicInfo setBasicOpen={setBasicOpen} />
-				) : (
-					<>
-						<div style={{ marginBottom: '25px' }}>
-							<div>
-								<a onClick={() => setBasicOpen(true)}>Edit</a>
-							</div>
-							<Title level={5} style={{ fontSize: '14px', color: '#6a6a6a' }}>
-								Address
-							</Title>
-							<Paragraph style={{ color: '#363636' }}>
-								{address.address2
-									? address.address + ' ' + address.address2
-									: address.address}
-								<br />
-								{`${address.city}, ${address.stateProvince} ${address.zip}`}
-								<br />
-								{address.country}
-							</Paragraph>
-						</div>
-						<div style={{ marginBottom: '25px' }}>
-							<Title level={5} style={{ fontSize: '14px', color: '#6a6a6a' }}>
-								Email
-							</Title>
-							<Paragraph style={{ color: '#363636' }}>
-								{basicInfo.email}
-							</Paragraph>
-						</div>
-						<div style={{ marginBottom: '25px' }}>
-							<Title level={5} style={{ fontSize: '14px', color: '#6a6a6a' }}>
-								Mobile
-							</Title>
-							<Paragraph style={{ color: '#363636' }}>
-								{getFullNumber(basicInfo.phonePrefix, basicInfo.phone)}
-							</Paragraph>
-						</div>
-					</>
-				)} */}
-				<Divider />
-				{demoOpen ? (
-					<DemographicsForm setDemoOpen={setDemoOpen} />
-				) : (
-					<div>
-						<Title
-							level={4}
-							style={{
-								color: '#2b2b2b',
-								fontSize: '18px',
-								display: 'flex',
-								flexDirection: 'row',
-								alignItems: 'center',
-							}}
-						>
-							Demographics{' '}
-							<span style={{ fontSize: '14px', marginLeft: '5px' }}>
-								(optional)
-							</span>
-						</Title>
-						<div>
-							<a onClick={() => setDemoOpen(true)}>Edit</a>
-						</div>
-						{demographics.share === '0' ? (
-							<Paragraph>
-								You've chosen not to share your demographics.
-							</Paragraph>
-						) : (
-							<>
-								<div>
-									<Title
-										level={5}
-										style={{ fontSize: '14px', color: '#6a6a6a' }}
-									>
-										Sex
-									</Title>
-									<Paragraph style={{ color: '#363636' }}>
-										{demographics.sex}
-									</Paragraph>
-								</div>
-								<div>
-									<Title
-										level={5}
-										style={{ fontSize: '14px', color: '#6a6a6a' }}
-									>
-										{' '}
-										Ethnicity{' '}
-									</Title>
-									<Paragraph style={{ color: '#363636' }}>
-										{getEthnicity(demographics.ethnicity)}
-									</Paragraph>
-								</div>
-								<div>
-									<Title
-										level={5}
-										style={{ fontSize: '14px', color: '#6a6a6a' }}
-									>
-										{' '}
-										Race{' '}
-									</Title>
-									{demographics?.race.map((element) => (
-										<Paragraph style={{ color: '#363636' }} key={element}>
-											{element}
-										</Paragraph>
-									))}
-								</div>
-								<Title level={5} style={{ fontSize: '14px', color: '#6a6a6a' }}>
-									{' '}
-									Disabilities/Serious Health Condition{' '}
-								</Title>
-								{demographics?.disability.map((condition) => (
-									<Paragraph style={{ color: '#363636' }} key={condition}>
-										{condition}
-									</Paragraph>
-								))}
-							</>
-						)}
-					</div>
-				)}
-			</div>
+			{contentObject[activeTab]}
 		</Card>
 	);
 };
--- src/containers/Profile/ApplicantCard/Tabs/BasicInfoTab.js
+++ src/containers/Profile/ApplicantCard/Tabs/BasicInfoTab.js
@@ -1,43 +1,22 @@
-import { useEffect, useState, useContext } from 'react';
-import { Avatar, Card, message, Typography, Divider } from 'antd';
+import { useState, useContext } from 'react';
+import { Typography, Divider } from 'antd';
 const { Paragraph, Title } = Typography;
 import ProfileContext from '../../Util/FormContext';
-import editableBasicInfo from '../../Forms/EditableBasicInfo/EditableBasicInfo';
+import EditableBasicInfo from '../../Forms/EditableBasicInfo/EditableBasicInfo';
 
 const BasicInfoTab = () => {
 
     const [basicOpen, setBasicOpen] = useState(false);
     const contextValue = useContext(ProfileContext);
     const { profile } = contextValue;
-    const { basicInfo, demographics } = profile;
+    const { basicInfo } = profile;
 	const address = basicInfo?.address;
-    
-    const getEthnicity = (value) => {
-		switch (value) {
-			case '1':
-				return 'Hispanic or Latino';
-			case '0':
-				return 'Not Hispanic or Latino';
-			default:
-				return '';
-		}
-	};
 
 	const getFullNumber = (prefix, number) => {
 		const areaCode = number.slice(0, 3);
 		const firstHalf = number.slice(3, 6);
 		const secondHalf = number.slice(6);
 		return `${prefix} (${areaCode}) ${firstHalf} - ${secondHalf}`;
-	};
-
-	const getFirstInitial = (first) => {
-		const firstName = first.split('');
-		return firstName[0];
-	};
-
-	const getLastInitial = (last) => {
-		const lastName = last.split('');
-		return lastName[0];
 	};
 
     return(
@@ -81,8 +60,6 @@ const BasicInfoTab = () => {
                     </div>
                 </>
             )}
-            <Divider />
-
         </div>
     );
 };
