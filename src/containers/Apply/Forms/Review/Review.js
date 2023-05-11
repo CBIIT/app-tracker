db74f619e7bdf50506fd9c4d99d5af0ecaa086b1
micahhooverNIH <micah.hoover@nih.com>
2023-05-11 13:57:13 -0400

    Added demographics data to review screen and a minor defensive check

--- src/containers/Apply/Forms/Review/Review.js
+++ src/containers/Apply/Forms/Review/Review.js
@@ -44,6 +44,79 @@ const review = (props) => {
 		...reference,
 	}));
 
+	const getAllRaces = (value) => {
+		for(var i = 0; i < value.length; i++)
+			value[i] = getRace(value[i]);
+		return value.join(', ');
+	}
+
+	const getAllDisabilities = (value) => {
+		for(var i = 0; i < value.length; i++)
+			value[i] = getDisability(value[i]);
+		return value.join(', ');
+	}
+
+	const getEthnicity = (value) => {
+		switch (value) {
+			case '1':
+				return 'Hispanic or Latino';
+			case '0':
+				return 'Not Hispanic or Latino';
+			default:
+				return '';
+		}
+	};
+
+    const getDisability = (value) => {
+			switch (value) {
+				case 'Deaf':
+					return 'Deaf or serious difficulty hearing';
+				case 'Blind':
+					return 'Blind or serious difficulty seeing even when wearing glasses';
+				case 'Amputee':
+					return 'Missing an arm, leg, hand or foot';
+				case 'Paralysis':
+					return 'Paralysis: partial or complete paralysis (any cause)';
+				case 'Disfigurement':
+					return 'Significant disfigurement: for example, severe disfigurements caused by burns, wounds, accidents or congenital disorders';
+				case 'Mobility Impairment':
+					return 'Significant mobility impairment: for example, uses a wheelchair, scooter, walker or uses a leg brace to walk';
+				case 'Psychiatric Disorder':
+					return 'Significant psychiatric disorder: for example, bipolar disorder, schizophrenia, PTSD or major depression';
+				case 'Intellectual Disability':
+					return 'Intellectual disability (formerly described as mental retardation)';
+				case 'Developmental Disability':
+					return 'Developmental disability: for example, cerebral palsy or autism spectrum disorder';
+				case 'Brain Injury':
+					return 'Traumatic brain injury';
+				case 'Dwarfism':
+					return 'Dwarfism';
+				case 'Epilepsy':
+					return 'Epilepsy';
+				case 'Other Disability':
+					return 'Other disability or serious health condition: for example, diabetes, cancer, cardiovascular disease, anxiety disorder or HIV infection';
+				case 'None':
+					return 'None of the conditions listed above apply to me.';
+				case 'Do Not Wish to Answer':
+					return 'I do not wish to answer questions regarding my disability/health conditions.';
+			}
+		};
+
+    const getRace = (value) => {
+        switch (value) {
+            case 'American Indian':
+                return "American Indian or Alaska Native";
+            case 'Asian':
+                return 'Asian';
+            case 'African-American':
+                return "Black or African-American";
+            case 'Pacific Islander':
+                return "Native Hawaiian or other Pacific Islander"
+            case 'White':
+                return 'White';
+        }
+    }
+
 	return (
 		<div>
 			<SectionHeader
@@ -127,6 +200,35 @@ const review = (props) => {
 						value={formData.address.stateProvince}
 					/>
 					<LabelValuePair label='Post Code' value={formData.address.zip} />
+				</div>
+			</div>
+			<SectionHeader
+				title='Demographics'
+				onClick={() => props.onEditButtonClick('demographics')}
+				showButton='false'
+			/>
+			<div className='SectionContent'>
+				<div className='SectionContentRow'>
+					<LabelValuePair
+						label='Sharing demographics'
+						value={formData?.questions?.share ? "Yes" : "No" }
+					/>
+					<LabelValuePair
+						label='Sex'
+						value={ formData?.questions?.sex ? formData?.questions?.sex : "Prefer not to answer"}
+					/>
+					<LabelValuePair
+						label='Ethnicity'
+						value={formData?.questions?.ethnicity ? getEthnicity(formData?.questions?.ethnicity) : "Prefer not to answer"}
+					/>
+					<LabelValuePair
+						label='Race'
+						value={getAllRaces(formData?.questions?.race)}
+					/>
+					<LabelValuePair
+						label='Disability'
+						value={getAllDisabilities(formData?.questions?.disability)}
+					/>
 				</div>
 			</div>
 			{formData.references.length > 0 ? (
