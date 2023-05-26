fad49c447305ee9c8468c998a47dd004afea3744
micahhooverNIH <micah.hoover@nih.com>
2023-05-26 14:15:18 -0400

    pull info from bre's new user tenant field to display stadtman dependent content

--- src/containers/CreateVacancy/Forms/BasicInfo/BasicInfo.js
+++ src/containers/CreateVacancy/Forms/BasicInfo/BasicInfo.js
@@ -1,5 +1,5 @@
 import { Form, Input, Slider, DatePicker, Tooltip, Checkbox } from 'antd';
-
+import useAuth from '../../../../hooks/useAuth';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import RequiredDocsList from './RequiredDocsList/RequiredDocsList';
@@ -13,6 +13,9 @@ const basicInformation = (props) => {
 	const formInstance = props.formInstance;
 	const initialValues = props.initialValues;
 	const readOnly = props.readOnly;
+
+	const { auth } = useAuth();
+	const { user } = auth;
 
 	const sliderMarks = {
 		0: '0',
@@ -108,7 +111,7 @@ const basicInformation = (props) => {
 				</div>
 			</div>
 
-			{ (true) ?	// TODO: replace "true" with an auth / user indicator that is true if the vacancy manager is stadman
+			{ (user?.tenant.trim().toLowerCase() === "stadtman") ?	// TODO: replace "true" with an auth / user indicator that is true if the vacancy manager is stadman
 				<Form.Item
 					label='Focus Area Selection'
 					name='requireFocusArea'
