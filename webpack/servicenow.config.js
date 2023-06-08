ccd08789d0dde66c5b093d067e58f67b4f63ba72
Harvey <harveybr@nci-02277589-ml.lan>
2023-06-08 11:25:02 -0500

    removing test user info

--- webpack/servicenow.config.js
+++ webpack/servicenow.config.js
@@ -15,12 +15,12 @@ const servicenowConfig = {
    * it is being used for sending REST calls in DEVELOPMENT mode only
    * no need to provide credentials for PRODUCTION
    */
-  REACT_APP_USER: 'breania.harvey.test.user@gmail.com',
+  REACT_APP_USER: '',
   /**
    * 
    * User password, for DEVELOPMENT mode only
    */
-  REACT_APP_PASSWORD: ')p-RD&wMgkJt*C]Ikv}${r}5A6(yH*aJ=F',
+  REACT_APP_PASSWORD: '',
   /**
    * 
    * ServiceNow path to GET resource which serves javascript files
