e7cee5aff066b5e8cefeb28ad6bea8b450d0ff77
Bre Harvey <bre.harvey@nih.gov>
2023-07-31 11:32:32 -0500

    phone and email validation on basic info profile tab

--- src/components/UI/EditableField/EditableField.js
+++ src/components/UI/EditableField/EditableField.js
@@ -1,14 +1,32 @@
 import './EditableField.css';
 import { Input, Form } from 'antd';
 
-const editableField = (props) => (
-	<Form.Item
-		label={props.label}
-		name={props.name}
-		rules={[{ required: props.required, message: 'Please provide an answer.' }]}
-	>
-		<Input />
-	</Form.Item>
-);
+const editableField = (props) =>
+	props.name == 'email' ? (
+		<Form.Item
+			label={props.label}
+			name={props.name}
+			rules={[
+				{ required: props.required, message: 'Please provide an answer.' },
+				{
+					type: 'string',
+					pattern: new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/),
+					message: 'Please provide a valid email address.',
+				}
+			]}
+		>
+			<Input />
+		</Form.Item>
+	) : (
+		<Form.Item
+			label={props.label}
+			name={props.name}
+			rules={[
+				{ required: props.required, message: 'Please provide an answer.' },
+			]}
+		>
+			<Input />
+		</Form.Item>
+	);
 
 export default editableField;
