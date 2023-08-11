f97cdc367a93fe5c6afe5e2b8cfe0bce5b5945fd
micahhooverNIH <micah.hoover@nih.com>
2023-08-11 14:47:57 -0400

    Adjusted Support callback before adding modal

--- src/components/Footer/ExternalNav/ExternalNav.js
+++ src/components/Footer/ExternalNav/ExternalNav.js
@@ -2,59 +2,70 @@ import React from 'react';
 
 import './ExternalNav.css';
 
-const externalNav = () => (
-	<div className='ExternalNav'>
-		<div className='ExternalNavContent'>
-			<nav>
-				<ul>
-					<li>
-						<a href='https://ccr.cancer.gov/'>Home</a>
-					</li>
-					<li>
-						<a href='mailto:NCIAppSupport@mail.nih.gov'>Support</a>
-					</li>
-					<li>
-						<a href='https://www.cancer.gov/global/web/policies'>Policies</a>
-					</li>
-					<li>
-						<a href='https://www.cancer.gov/global/web/policies/accessibility'>
-							Accessibility
-						</a>
-					</li>
-					<li>
-						<a href='https://cancer.gov/global/viewing-files'>Viewing Files</a>
-					</li>
-					<li>
-						<a href='https://www.cancer.gov/global/web/policies/foia'>FOIA</a>
-					</li>
-					<li>
-						<a href='https://www.hhs.gov/vulnerability-disclosure-policy/index.html'>
-							HHS Vulnerability Disclosure
-						</a>
-					</li>
-				</ul>
-				<ul>
-					<li>
-						<a href='https://www.hhs.gov/'>
-							U.S Department of Health and Human Services
-						</a>
-					</li>
-					<li>
-						<a href='https://www.nih.gov/'>National Institutes of Health</a>
-					</li>
-					<li>
-						<a href='https://www.cancer.gov/'>National Cancer Institute</a>
-					</li>
-					<li>
-						<a href='https://usa.gov/'>USA.gov</a>
-					</li>
-				</ul>
-			</nav>
+const externalNav = () => {
+
+	const displayDialog = (link) => {
+		alert('groovy baby YEAH !')
+		//setShowProfileDialog(true);
+	};
+
+	return (
+		<div className='ExternalNav'>
+			<div className='ExternalNavContent'>
+				<nav>
+					<ul>
+						<li>
+							<a href='https://ccr.cancer.gov/'>Home</a>
+						</li>
+						{/* <li>
+							<a href='mailto:NCIAppSupport@mail.nih.gov'>Support</a>
+						</li> */}
+						<li>
+							<a onClick={() => displayDialog()}>Support</a>
+						</li>
+						<li>
+							<a href='https://www.cancer.gov/global/web/policies'>Policies</a>
+						</li>
+						<li>
+							<a href='https://www.cancer.gov/global/web/policies/accessibility'>
+								Accessibility
+							</a>
+						</li>
+						<li>
+							<a href='https://cancer.gov/global/viewing-files'>Viewing Files</a>
+						</li>
+						<li>
+							<a href='https://www.cancer.gov/global/web/policies/foia'>FOIA</a>
+						</li>
+						<li>
+							<a href='https://www.hhs.gov/vulnerability-disclosure-policy/index.html'>
+								HHS Vulnerability Disclosure
+							</a>
+						</li>
+					</ul>
+					<ul>
+						<li>
+							<a href='https://www.hhs.gov/'>
+								U.S Department of Health and Human Services
+							</a>
+						</li>
+						<li>
+							<a href='https://www.nih.gov/'>National Institutes of Health</a>
+						</li>
+						<li>
+							<a href='https://www.cancer.gov/'>National Cancer Institute</a>
+						</li>
+						<li>
+							<a href='https://usa.gov/'>USA.gov</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div className='ExternalNavTrademark'>
+				NIH ... Turning Discovery Into Health ®
+			</div>
 		</div>
-		<div className='ExternalNavTrademark'>
-			NIH ... Turning Discovery Into Health ®
-		</div>
-	</div>
-);
+	);
+};
 
 export default externalNav;
