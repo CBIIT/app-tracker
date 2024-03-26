7a0b862f55ceed14b697d6d40182f2e711893e14
Bre Harvey <bre.harvey@nih.gov>
2024-03-26 11:59:26 -0500

    removing user information

--- webpack/servicenow.config.js
+++ webpack/servicenow.config.js
@@ -15,12 +15,12 @@ const servicenowConfig = {
    * it is being used for sending REST calls in DEVELOPMENT mode only
    * no need to provide credentials for PRODUCTION
    */
-  REACT_APP_USER: 'john.lechuga.test.user@gmail.com',
+  REACT_APP_USER: '',
   /**
    * 
    * User password, for DEVELOPMENT mode only
    */
-  REACT_APP_PASSWORD: 'q{V{T1ZlLNJPm3Pc',
+  REACT_APP_PASSWORD: '',
   /**
    * 
    * ServiceNow path to GET resource which serves javascript files
