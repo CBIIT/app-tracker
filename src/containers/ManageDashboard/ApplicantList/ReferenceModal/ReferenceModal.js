5ba202d364ce817e0c70ebdc219eeeab130ea38f
Bre Harvey <bre.harvey@nih.gov>
2024-04-08 16:03:10 -0500

    Fixing build errors

--- src/containers/ManageDashboard/ApplicantList/ReferenceModal/ReferenceModal.js
+++ src/containers/ManageDashboard/ApplicantList/ReferenceModal/ReferenceModal.js
@@ -3,43 +3,43 @@ const { Paragraph } = Typography;
 import { WarningOutlined } from '@ant-design/icons';
 
 const referenceModal = (props) => {
-    const sendReferences = props.sendReferences;
+	const sendReferences = props.sendReferences;
 
-    const handleReferenceSubmit = () => {
+	const handleReferenceSubmit = () => {
 		sendReferences(props.appSysId);
 		props.setShowModal(false);
-	}
+	};
 
 	const handleReferenceCancel = () => {
 		props.setShowModal(false);
-	}
+	};
 
-    return (
-        <Modal 
-				title={ 
-					<Paragraph>
-						<WarningOutlined />
-						{" "}Reference Notifications Have Been Sent
-					</Paragraph>
-				}
-				open={props.showModal}
-				onOk={handleReferenceSubmit}
-				onCancel={handleReferenceCancel}
-				closable={false}
-				footer={[
-					<Button key='modal-button' onClick={handleReferenceSubmit}>
-						Send References Again
-					</Button>,
-					<Button key='modal-continue' onClick={handleReferenceCancel}>
-						Cancel
-					</Button>
-				]}
-			>
+	return (
+		<Modal
+			title={
 				<Paragraph>
-					Notifications to this applicant's selected references have already been sent. Would you like to send the notifications again?
+					<WarningOutlined /> Reference Notifications Have Been Sent
 				</Paragraph>
-			</Modal>
-    );
+			}
+			open={props.showModal}
+			onOk={handleReferenceSubmit}
+			onCancel={handleReferenceCancel}
+			closable={false}
+			footer={[
+				<Button key='modal-button' onClick={handleReferenceSubmit}>
+					Send References Again
+				</Button>,
+				<Button key='modal-continue' onClick={handleReferenceCancel}>
+					Cancel
+				</Button>,
+			]}
+		>
+			<Paragraph>
+				Notifications to this applicant's selected references have already been
+				sent. Would you like to send the notifications again?
+			</Paragraph>
+		</Modal>
+	);
 };
 
 export default referenceModal;
