5fb3b42a3faca47dffde09f37ed963987400b12e
Bre Harvey <bre.harvey@nih.gov>
2024-04-18 16:05:00 -0500

    removing unused code for build

--- src/containers/Apply/Forms/Review/Review.js
+++ src/containers/Apply/Forms/Review/Review.js
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { Table } from 'antd';
 import { FileTextOutlined } from '@ant-design/icons';
 
-import { displayReferenceContactQuestion } from '../../../../components/Util/Application/Application';
 import FormContext from '../../Context';
 import SectionHeader from '../../../../components/UI/ReviewSectionHeader/ReviewSectionHeader';
 import LabelValuePair from '../../../../components/UI/LabelValuePair/LabelValuePair';
