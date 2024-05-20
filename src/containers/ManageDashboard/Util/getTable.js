63d4d5c1c76902e635b0cc4278fb22436058b820
Bre Harvey <bre.harvey@nih.gov>
2024-05-20 11:38:43 -0500

    getTable update

--- src/containers/ManageDashboard/Util/getTable.js
+++ src/containers/ManageDashboard/Util/getTable.js
@@ -4,55 +4,40 @@ import { TRIAGE, SCORING, IN_REVIEW, REVIEW_COMPLETE, COMPLETED } from "../../..
 import { GET_ROLLING_APPLICANT_LIST } from '../../../constants/ApiEndpoints';
 
 // ROLLING CLOSE GETTABLE 
-export const getTable = (filter, userRoles, userCommitteeRole, applicantColumns, committeeColumns) => {
-        // set table to table for rolling close?
-        // IF user is a vacancy manager
-        if (userRoles.includes(OWM_TEAM)) {
-            switch (filter) {
-                case SCORING:
-                case IN_REVIEW:
-                case COMPLETED:
-                default:
-                    return table
-            }
-        } else if (userCommitteeRole === COMMITTEE_CHAIR) {
-            switch (filter) {
-                case SCORING:
-                case IN_REVIEW:
-                case COMPLETED:
-            }
-        } else if (userCommitteeRole === COMMITTEE_MEMBER_VOTING || userCommitteeRole === COMMITTEE_MEMBER_NON_VOTING) {
-
-        } else {
-
+export const getTable = (filter, userRoles, userCommitteeRole, applicants, applicantColumns, committeeColumns) => {
+    // set table to table for rolling close?
+    // IF user is a vacancy manager
+    if (userRoles.includes(OWM_TEAM)) {
+        switch (filter) {
+            case SCORING:
+                // return IndividualScoringTable: recommended and non-recommended
+            case IN_REVIEW:
+            case COMPLETED:
+                // return IndividualScoringTable w/ diff props: recommended and non-recommended
+            default:
+                return table
         }
-            // SWITCH filter
-                // CASE scoring
-                // CASE in review
-                // CASE selected
-                // default return table
-        // ELSE IF user is committee chair
-            // SWITCH filter
-                // CASE scoring
-                // CASE in review
-                // CASE completed
-                // default return table
-        // ELSE IF user is committee member
-            // return committee member applicant list
-        // ELSE return table
+    } else if (userCommitteeRole === COMMITTEE_CHAIR) {
+        switch (filter) {
+            case SCORING:
+            case IN_REVIEW:
+            case COMPLETED:
+        }
+    } else if (userCommitteeRole === COMMITTEE_MEMBER_VOTING || userCommitteeRole === COMMITTEE_MEMBER_NON_VOTING) {
 
-        // Needed variables:
-            // imported roles
-            // applicant columns
-            // committee columns
-            // imported table component
-            // applicants
-            // tablePagination
-            // tableLoading
-            // loadAllApplicants function
-            // vacancy states
-            // recommendedApplicants
-            // loadRecommendedApplicants
-            // referenceCollection
-            // loadVacancyAndApplicants function
+    } else {
+        return table;
+    }
+
+    // Needed variables:
+        // imported roles
+        // imported table component
+        // tablePagination
+        // tableLoading
+        // loadAllApplicants function
+        // vacancy states
+        // recommendedApplicants
+        // loadRecommendedApplicants
+        // referenceCollection
+        // loadVacancyAndApplicants function
 };
