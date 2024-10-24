a71fcc98309607a5579ce627e0fc7b23cef7fecb
Lechuga <lechugajm@nih.gov>
2024-10-24 13:44:50 -0500

    initial test file set up

---
+++ src/containers/ViewVacancyDetails/ViewVacancyDetails.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ViewVacancyDetails from './ViewVacancyDetails';
+import Header from './Header/Header';
+import Divider from './Divider/Divider';
+
