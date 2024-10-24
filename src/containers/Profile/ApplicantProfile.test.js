73e299120ac6f3e9f4e13b71c59928520106ec98
Lechuga <lechugajm@nih.gov>
2024-10-24 11:49:32 -0500

    initial test file set up for ApplicantProfile

---
+++ src/containers/Profile/ApplicantProfile.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ApplicantProfile from './ApplicantProfile';
+import ApplicantCard from './ApplicantCard/ApplicantCard';
+import EditableBasicInfo from './Forms/EditableBasicInfo/EditableBasicInfo';
+import Demographics from './Forms/Demographics/Demographics';
