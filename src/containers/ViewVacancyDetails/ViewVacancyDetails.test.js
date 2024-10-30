76096a1d46930e8b4797800f38d29f4d9c56518f
Lechuga <lechugajm@nih.gov>
2024-10-30 10:59:53 -0500

    initial mock set up

--- src/containers/ViewVacancyDetails/ViewVacancyDetails.test.js
+++ src/containers/ViewVacancyDetails/ViewVacancyDetails.test.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import ViewVacancyDetails from './ViewVacancyDetails';
 import Header from './Header/Header';
 import Divider from './Divider/Divider';
+import useAuth from '../../hooks/useAuth';
+import { describe } from 'optimist';
 
+jest.mock('axios');
+jest.mock('react-router-dom', () => ({
+    useParams: jest.fn(),
+}));
+jest.mock('../../hooks/useAuth', () => ({
+    __esModule: true,
+    default: jest.fn(), 
+}));
+jest.mock('antd', () => ({
+    ...jest.requireActual('antd'),
+    message: {
+        error: jest.fn(),
+    }
+}));
+
+describe('ViewVacancyDetails', () => {});
