d9457fbbe1c234dd1338ad0d6b9d4259b5ed8b20
Lechuga <lechugajm@nih.gov>
2024-11-06 09:35:50 -0600

    initial test file set up

---
+++ src/containers/Apply/Apply.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import axios from 'axios';
+import { message } from 'antd';
+import Apply from './Apply';
+import useAuth from '../../hooks/useAuth';
