f9c297455db1c7890f6ef55593ba6f2dc5c14005
Lechuga <lechugajm@nih.gov>
2024-11-06 10:14:06 -0600

    initial mocking of hooks and integrations

--- src/containers/Apply/Apply.test.js
+++ src/containers/Apply/Apply.test.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
 import { message } from 'antd';
 import Apply from './Apply';
 import useAuth from '../../hooks/useAuth';
+
+jest.mock('axios');
+jest.mock('react-router-dom', () => ({
+    useParams: jest.fn(),
+    useHistory: jest.fn(),
+}));
+jest.mock('../../hooks/useAuth', () => ({
+    _esModule: true,
+    default: jest.fn(),
+}));
+jest.mock('antd', () => ({
+    ...jest.requireActual('antd'),
+    message: {
+        error: jest.fn(),
+    },
+}));
+
