a771bbfdc11eab7495487031a34949e6f85af789
webbmj-nih <michael.webb@nih.gov>
2025-01-15 15:41:57 -0500

    Added Additional Test for UserPicker.js

--- src/components/UI/UserPicker/UserPicker.test.js
+++ src/components/UI/UserPicker/UserPicker.test.js
@@ -15,11 +15,22 @@ describe('UserPicker Component', () => {
         organization: { value: 'Example Org' },
         sys_id: { value: '1' }
     };
-
+    const mockUser2 = {
+        name: { value: 'John Doe2' },
+        email: { value: 'john.doe2@example.com' },
+        organization: { value: 'Example Org Two' },
+        sys_id: { value: '2' }
+    };
+    const mockUser3 = {
+        name: { value: 'Jane Doe' },
+        email: { value: 'jane.doe@example.com' },
+        organization: { value: 'Example Org Three' },
+        sys_id: { value: '3' }
+    };
     beforeEach(() => {
         axios.get.mockResolvedValue({
             data: {
-                result: [mockUser]
+                result: [mockUser, mockUser2, mockUser3]
             }
         });
     });
@@ -38,8 +49,10 @@ describe('UserPicker Component', () => {
         const input = screen.getByRole('textbox');
         fireEvent.change(input, { target: { value: 'John' } });
 
-        const options = await screen.findAllByText('John Doe');
-        expect(options).toHaveLength(1);
+        await screen.findAllByText('John Doe');
+        expect(screen.getByText('John Doe')).toBeInTheDocument();
+        expect(screen.getByText('john.doe@example.com')).toBeInTheDocument();
+        expect(screen.getByText('Example Org')).toBeInTheDocument();
     });
 
     test('calls onChange when an option is selected', async () => {
@@ -61,5 +74,24 @@ describe('UserPicker Component', () => {
         expect(screen.getByText('John Doe')).toBeInTheDocument();
         expect(screen.getByText('john.doe@example.com')).toBeInTheDocument();
         expect(screen.getByText('Example Org')).toBeInTheDocument();
+    });
+    test('shows appropriate users appear when input is changed', async () => {
+        render(<UserPicker value={mockUser} onChange={mockOnChange} />);
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, { target: { value: 'John' } });
+        await screen.findAllByText('John Doe');
+        expect(screen.queryByText('Jane Doe')).not.toBeInTheDocument();
+        expect(screen.getByText('John Doe')).toBeInTheDocument();
+        expect(screen.getByText('john.doe@example.com')).toBeInTheDocument();
+        expect(screen.getByText('Example Org')).toBeInTheDocument();
+        expect(screen.getByText('John Doe2')).toBeInTheDocument();
+
+        fireEvent.change(input, { target: { value: 'Jane' } });
+        await screen.findAllByText('Jane Doe');
+        expect(screen.getByText('Jane Doe')).toBeInTheDocument();
+        expect(screen.getByText('jane.doe@example.com')).toBeInTheDocument();
+        expect(screen.getByText('Example Org Three')).toBeInTheDocument();
+        expect(screen.queryByText('John Doe')).not.toBeInTheDocument();
+        expect(screen.queryByText('John Doe2')).not.toBeInTheDocument();
     });
 });
