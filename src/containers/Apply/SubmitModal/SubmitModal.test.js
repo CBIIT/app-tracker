f56d1574ce9d5784de3ed5a81d14a0ee8b5b4a40
Lechuga <lechugajm@nih.gov>
2025-01-17 13:48:51 -0600

    initial submit modal test file creation

---
+++ src/containers/Apply/SubmitModal/SubmitModal.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import SubmitModal from './SubmitModal';
+
+// Test cases for new app submissions
+
+
+// Test cases for editing submitted applications
