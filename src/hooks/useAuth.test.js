5a32fb580e83bb3c8e6f09be973113fa6ec0a5ff
webbmj-nih <michael.webb@nih.gov>
2025-02-13 11:34:35 -0500

    Added Tests for useAuth.js

---
+++ src/hooks/useAuth.test.js
@@ -0,0 +1,19 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useAuth from './useAuth';
+import AuthContext from '../context/AuthContext';
+import React from 'react';
+
+describe('useAuth', () => {
+    it('should use AuthContext', () => {
+        const mockAuthContextValue = { user: 'testUser' };
+        const wrapper = ({ children }) => (
+            <AuthContext.Provider value={mockAuthContextValue}>
+                {children}
+            </AuthContext.Provider>
+        );
+
+        const { result } = renderHook(() => useAuth(), { wrapper });
+
+        expect(result.current).toBe(mockAuthContextValue);
+    });
+});
