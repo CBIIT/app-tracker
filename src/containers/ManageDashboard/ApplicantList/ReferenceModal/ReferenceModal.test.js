d4c76361e7529a8e6cc037072f509d822770a2ad
Lechuga <lechugajm@nih.gov>
2025-02-26 10:51:33 -0600

    initial test file setup for Reference Modal

---
+++ src/containers/ManageDashboard/ApplicantList/ReferenceModal/ReferenceModal.test.js
@@ -0,0 +1,7 @@
+import { describe } from 'optimist';
+import ReferenceModal from './ReferenceModal';
+import { render, screen } from '@testing-library/react';
+
+describe('ReferenceModal', () => {
+    
+});
