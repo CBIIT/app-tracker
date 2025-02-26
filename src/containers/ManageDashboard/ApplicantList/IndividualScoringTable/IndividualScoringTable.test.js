de607b72799269d88e45c72d5d5c497b3e76ece2
Lechuga <lechugajm@nih.gov>
2025-02-26 14:20:39 -0600

    attempting to render Individual Scoring Table

--- src/containers/ManageDashboard/ApplicantList/IndividualScoringTable/IndividualScoringTable.test.js
+++ src/containers/ManageDashboard/ApplicantList/IndividualScoringTable/IndividualScoringTable.test.js
@@ -1,13 +1,43 @@
-import individualScoringTable from "./IndividualScoringTable";
+import individualScoringTable from './IndividualScoringTable';
 import { render, screen } from '@testing-library/react';
+import {
+	mockRecommendedApplicants,
+	mockRecommendedApplicantsTablePagination,
+} from './IndividualScoringTableMockData';
 
 describe('individualScoringTable', () => {
+	let mockRecommendedApplicantsTableLoading;
+	let mockReferenceCollection; // Mocks the props.referenceCollection
+	let mockLoadRecommendedApplicants;
+	let mockLoadVacancyAndApplicants;
 
-    beforeEach(() => {
+	beforeEach(() => {
+		mockRecommendedApplicantsTableLoading = false;
+		mockLoadRecommendedApplicants = jest.fn();
+		mockLoadVacancyAndApplicants = jest.fn();
+	});
 
-    });
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
 
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+	test('renders individualScoringTable component', () => {
+        mockReferenceCollection = true;
+        mockRecommendedApplicantsTableLoading = true;
+
+		render(
+			<individualScoringTable
+				applicants={mockRecommendedApplicants}
+				pagination={mockRecommendedApplicantsTablePagination}
+				loading={mockRecommendedApplicantsTableLoading}
+				onTableChange={mockLoadRecommendedApplicants}
+				refCollection={mockReferenceCollection}
+				isVacancyManager={true}
+				reloadVacancy={mockLoadVacancyAndApplicants}
+			/>
+		);
+
+        expect(screen.getByText('Collect References')).toBeInTheDocument();
+
+	});
 });
---
+++ src/containers/ManageDashboard/ApplicantList/IndividualScoringTable/IndividualScoringTableMockData.js
@@ -0,0 +1,44 @@
+export const mockRecommendedApplicants = [
+    {
+        actual_member_score: 0,
+        applicant_email: 'user@mail.com',
+        applicant_first_name: 'John',
+        applicant_last_name: 'Doe',
+        applicant_name: 'Doe, John',
+        chair_triage_comment: '',
+        chair_triage_status: 'yes',
+        committee_comments: '',
+        committee_decision: '--',
+        interview_recommendation: {
+            Maybe: 0,
+            No: 0,
+            Yes: 0,
+        },
+        is_app_complete: '',
+        maybe: 0,
+        no: 0,
+        references_sent: '0',
+        refferred_to_interview: null,
+        referred_to_selecting_official: null,
+        scores: {
+            scoring_status: 'Scoring in Progress',
+            selected: null,
+            state: 'scoring',
+            sys_id: '12345',
+            total: 0,
+            triage_comments: '',
+            triage_status: 'yes',
+            yes: 0,
+        },
+    }
+];
+
+export const mockRecommendedApplicantsTablePagination = {
+    hideOnSinglePage: true,
+    pageSize: 10,
+    pagesizeOptions: [
+        10,
+        25,
+    ],
+    total: 0,
+};
