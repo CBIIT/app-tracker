62eec22b6013017be597810433be3cce6cb7dacf
Lechuga <lechugajm@nih.gov>
2025-03-19 14:45:31 -0500

    cleaned up mandatory statement test file

--- src/containers/CreateVacancy/Forms/MandatoryStatements/MandatoryStatements.test.js
+++ src/containers/CreateVacancy/Forms/MandatoryStatements/MandatoryStatements.test.js
@@ -1,99 +1,84 @@
 import MandatoryStatements from './MandatoryStatements';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import {
-	mockBasicInfo,
-	mockEmailTemplates,
-	mockMandatoryStatements,
-	mockVacancyCommittee,
-} from './MandatoryStatementsMockData';
+import { mockMandatoryStatements } from './MandatoryStatementsMockData';
 import { Form } from 'antd';
-
-const { result } = jest.mock('antd', () => {
-	return {
-		...mockAntd,
-		mockForm: {
-			...mockForm,
-			useForm: () => [
-				{
-					...mockResult.current[0],
-					mockGetFieldError: jest.fn(),
-					mockGetFieldInstace: jest.fn(),
-					mockGetFieldValue: jest.fn().mockReturnValue(true),
-					mockGetFieldWarning: jest.fn(),
-					mockGetFieldsError: jest.fn(),
-					mockGetFieldsValue: jest.fn(),
-					mockGetInternalHooks: jest.fn(),
-					mockIsFieldTouched: jest.fn(),
-					mockIsFieldValidating: jest.fn(),
-					mockIsFieldsTouched: jest.fn(),
-					mockIsFieldsValidating: jest.fn(),
-					mockResetFields: jest.fn(),
-					mockScrollToField: jest.fn(),
-					mockSetFieldValue: jest.fn(),
-					mockSetFields: jest.fn(),
-					mockSetFieldsValue: jest.fn(),
-					mockSubmit: jest.fn(),
-					mockValidateFields: jest.fn(),
-					__INTERNAL__: {
-						mockItemRef: jest.fn(),
-						name: 'MandatoryStatements',
-					},
-					_init: true,
-				},
-			],
-		},
-	};
-});
 
 jest.mock('../../../../components/UI/SwitchFormItem/SwitchFormItem', () => {
 	return function DummySwitchFormItem({ onChangeHandler }) {
-        return <input type="checkbox" data-testid="SwitchFormItemEditorSwitch" name="testName" label="Test Label" readOnly={false} onChange={(e) => onChangeHandler(e.target.checked)} />;
-    };
+		return (
+			<input
+				type='checkbox'
+				data-testid='SwitchFormItemEditorSwitch'
+				name='testName'
+				label='Test Label'
+				readOnly={false}
+				onChange={(e) => onChangeHandler(e.target.checked)}
+			/>
+		);
+	};
 });
 
-window.matchMedia = window.matchMedia || function () {
-    return {
-        matches: false,
-        addListener: function () { },
-        removeListener: function () { }
-    };
-};
+window.matchMedia =
+	window.matchMedia ||
+	function () {
+		return {
+			matches: false,
+			addListener: function () {},
+			removeListener: function () {},
+		};
+	};
 
 describe('MandatoryStatements', () => {
 	let mockRestrictedEditMode;
 	beforeEach(() => {
-	mockRestrictedEditMode = undefined;
-	window.document.getSelection = jest.fn()
-   });
-  
-   afterEach(() => {
-	jest.clearAllMocks();
-   });
-  
-   const FormWrapper = () => {
-	const [form] = Form.useForm();
-	 
-	return (<MandatoryStatements 
-	 initialValues={mockMandatoryStatements}
-	 formInstance={form}
-	 readOnly={mockRestrictedEditMode}
-	/>);
-   };
-   test('Should render all mandatory statements', () => {
-  
-		render (<FormWrapper />);
+		mockRestrictedEditMode = undefined;
+		window.document.getSelection = jest.fn();
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	const FormWrapper = () => {
+		const [form] = Form.useForm();
+
+		return (
+			<MandatoryStatements
+				initialValues={mockMandatoryStatements}
+				formInstance={form}
+				readOnly={mockRestrictedEditMode}
+			/>
+		);
+	};
+	
+	test('Should render all mandatory statements', () => {
+		render(<FormWrapper />);
 
 		waitFor(() => {
-			const equalOpportunityEmployerHeader = screen.getByText('Equal Employment Opportunity Policy');
-			const equalOpportunityEmployer = screen.getByText('<p>The United States government does not discriminate in employment on the basis of race, color, religion, sex, pregnancy, national origin, political affiliation, sexual orientation, marital status, disability, genetic information, age, membership in an employee organization, retaliation, parental status, military service or other non-merit factor.</p><br/><p>To learn more, please visit the <a href="https://www.eeoc.gov/federal-sector/federal-employees-job-applicants">U.S. Equal Employment Opportunity Commission</a>.</p>');
-			const standardsOfConductHeader = screen.getByText('Standards of Conduct/Financial Disclosure');
-			const standardsOfConduct = screen.getByText('<p>The National Institutes of Health inspires public confidence in our science by maintaining high ethical principles. NIH employees are subject to Federal government-wide regulations and statutes as well as agency-specific regulations described at the NIH Ethics Website. We encourage you to review this information. The position is subject to a background investigation and requires the incumbent to complete a public financial disclosure report prior to the effective date of the appointment.</p>')
+			const equalOpportunityEmployerHeader = screen.getByText(
+				'Equal Employment Opportunity Policy'
+			);
+			const equalOpportunityEmployer = screen.getByText(
+				'<p>The United States government does not discriminate in employment on the basis of race, color, religion, sex, pregnancy, national origin, political affiliation, sexual orientation, marital status, disability, genetic information, age, membership in an employee organization, retaliation, parental status, military service or other non-merit factor.</p><br/><p>To learn more, please visit the <a href="https://www.eeoc.gov/federal-sector/federal-employees-job-applicants">U.S. Equal Employment Opportunity Commission</a>.</p>'
+			);
+			const standardsOfConductHeader = screen.getByText(
+				'Standards of Conduct/Financial Disclosure'
+			);
+			const standardsOfConduct = screen.getByText(
+				'<p>The National Institutes of Health inspires public confidence in our science by maintaining high ethical principles. NIH employees are subject to Federal government-wide regulations and statutes as well as agency-specific regulations described at the NIH Ethics Website. We encourage you to review this information. The position is subject to a background investigation and requires the incumbent to complete a public financial disclosure report prior to the effective date of the appointment.</p>'
+			);
 			const foreignEducationHeader = screen.getByText('Foreign Education');
-			const foreignEducation = screen.getByText('<p>Applicants who have completed part or all of their education outside of the U.S. must have their foreign education evaluated by an accredited organization to ensure that the foreign education is equivalent to education received in accredited educational institutions in the United States. We will only accept the completed foreign education evaluation. For more information on foreign education verification, visit the https://www.naces.org website. Verification must be received prior to the effective date of the appointment.</p>');
-			const reasonableAccommodationHeader = screen.getByText('Reasonable Accommodation');
-			const reasonableAccommodation = screen.getByText('<p>NIH provides reasonable accommodations to applicants with disabilities. If you require reasonable accommodation during any part of the application and hiring process, please notify us. The decision on granting reasonable accommodation will be made on a case-by-case basis.</p>');
-		
+			const foreignEducation = screen.getByText(
+				'<p>Applicants who have completed part or all of their education outside of the U.S. must have their foreign education evaluated by an accredited organization to ensure that the foreign education is equivalent to education received in accredited educational institutions in the United States. We will only accept the completed foreign education evaluation. For more information on foreign education verification, visit the https://www.naces.org website. Verification must be received prior to the effective date of the appointment.</p>'
+			);
+			const reasonableAccommodationHeader = screen.getByText(
+				'Reasonable Accommodation'
+			);
+			const reasonableAccommodation = screen.getByText(
+				'<p>NIH provides reasonable accommodations to applicants with disabilities. If you require reasonable accommodation during any part of the application and hiring process, please notify us. The decision on granting reasonable accommodation will be made on a case-by-case basis.</p>'
+			);
+
 			expect(equalOpportunityEmployerHeader).toBeInTheDocument();
 			expect(equalOpportunityEmployer).toBeInTheDocument();
 			expect(standardsOfConductHeader).toBeInTheDocument();
@@ -103,8 +88,5 @@ describe('MandatoryStatements', () => {
 			expect(reasonableAccommodationHeader).toBeInTheDocument();
 			expect(reasonableAccommodation).toBeInTheDocument();
 		});
-
-   });
-
-
+	});
 });
