3fd97a6ff64b26620d26f50ae26a3506a7143d2c
Lechuga <lechugajm@nih.gov>
2025-05-12 12:41:59 -0500

    success/failed on save draft api call

--- src/containers/Apply/SubmitModal/SubmitAppWorkflow/SubmitNewApp.test.js
+++ src/containers/Apply/SubmitModal/SubmitAppWorkflow/SubmitNewApp.test.js
@@ -1,34 +1,83 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import { message } from 'antd';
 import SubmitNewApp from './SubmitNewApp';
 import {
-    mockSaveAppDraftResponse,
-    mockSaveAppDraftFailResponse,
-    mockSaveDraftDocResponse,
-    mockSaveDraftDocFailResponse,
-    mockOptions,
-    mockFile,
-    mockFileAttachResponse,
-    mockAttachmentCheckResponse,
-    mockAttachmentCheckFailResponse,
-    mockSubmitAppResponse,
-    mockSubmitAppFailResponse,
-    mockDocumentToDelete,
-    mockAttachmentDeleteResponse,
-    mockApplicationAttachmentCheckResponse,
-    mockApplicationUpdateResponse,
-    mockInfoToSend,
-    mockFormData,
-    mockInfoToSendEdit,
+	SAVE_APP_DRAFT,
+	CREATE_APP_DOCS,
+	SERVICE_NOW_FILE_ATTACHMENT,
+	SERVICE_NOW_ATTACHMENT,
+	ATTACHMENT_CHECK,
+	SUBMIT_APPLICATION,
+} from '../../../../constants/ApiEndpoints';
+import {
+	mockSaveAppDraftResponse,
+	mockSaveAppDraftFailResponse,
+	mockSaveDraftDocResponse,
+	mockSaveDraftDocFailResponse,
+	mockOptions,
+	mockFile,
+	mockFileAttachResponse,
+	mockAttachmentCheckResponse,
+	mockAttachmentCheckFailResponse,
+	mockSubmitAppResponse,
+	mockSubmitAppFailResponse,
+	mockDocumentToDelete,
+	mockAttachmentDeleteResponse,
+	mockApplicationAttachmentCheckResponse,
+	mockApplicationUpdateResponse,
+	mockInfoToSend,
+	mockFormData,
+	mockInfoToSendEdit,
 } from '../SubmitModalMockData';
 
+jest.mock('axios');
+
 describe('SubmitNewApp component', () => {
+	let mockSetPercent;
+	const mockSetAppSysId = jest.fn();
+	const mockSetConfirmLoading = jest.fn();
+	const mockCheckAuth = jest.fn();
+	const mockSetAuth = jest.fn();
+	const mockSetSubmitted = jest.fn();
 
-    beforeEach(() => {
+	beforeEach(() => {});
 
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	test('Should handle sucessful saveAppDraft function call', async () => {
+        axios.post.mockResolvedValueOnce(mockSaveAppDraftResponse);
+
+        const saveDraft = await axios.post(SAVE_APP_DRAFT, mockFormData);
+
+        expect(axios.post).toHaveBeenCalledTimes(1);
+        expect(axios.post).toHaveBeenCalledWith(SAVE_APP_DRAFT, mockFormData);
+        expect(saveDraft).toEqual(mockSaveAppDraftResponse);
+        // expect(mockSetPercent).toBe(20);
     });
 
-    afterEach(() => {
-        jest.clearAllMocks();
+    test('Should handle failed saveAppDraft function call', async () => {
+        axios.post.mockResolvedValueOnce(mockSaveAppDraftFailResponse);
+
+        const saveDraft = await axios.post(SAVE_APP_DRAFT, mockFormData);
+
+        expect(axios.post).toHaveBeenCalledTimes(1);
+        expect(axios.post).toHaveBeenCalledWith(SAVE_APP_DRAFT, mockFormData);
+        expect(saveDraft).toEqual(mockSaveAppDraftFailResponse);
+        waitFor (() => expect(screen.getAllByText("Sorry! There was an error when attempting to submit your application")).toBeInTheDocument());
+        // expect(mockSetSubmitted).toBe(false);
     });
-    
+
+    test('Should handle sucessful createAppDocs function call', async () => {
+        axios.post.mockResolvedValueOnce(mockSaveDraftDocResponse);
+
+        const saveDraft = await axios.post(CREATE_APP_DOCS, mockFormData);
+
+        expect(axios.post).toHaveBeenCalledTimes(1);
+        expect(axios.post).toHaveBeenCalledWith(CREATE_APP_DOCS, mockFormData);
+        expect(saveDraft).toEqual(mockSaveDraftDocResponse);
+        // expect(mockSetPercent).toBe(40);
+    });
 });
