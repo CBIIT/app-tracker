d47de538881d70743d3caebfd90e71d7b2929271
Lechuga <lechugajm@nih.gov>
2025-10-08 09:40:35 -0500

    passing test to ensure Focus Area does not display for non-stadtman applications

--- src/containers/ApplicantApplicationView/ApplicantApplicationView.test.js
+++ src/containers/ApplicantApplicationView/ApplicantApplicationView.test.js
@@ -1,9 +1,9 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import ApplicationApplicationView from './ApplicantApplicationView';
 import * as transformJsonFromBackend from './Util/TransformJsonFromBackend';
 import { useParams, MemoryRouter } from 'react-router-dom';
-import { 
-	mockProps, 
+import {
+	mockProps,
 	mockResponse,
 	mockStadtmanResponse,
 	mockStadtmanApplication,
@@ -19,25 +19,40 @@ jest.mock('react-router-dom', () => ({
 jest.mock('./Util/TransformJsonFromBackend', () => ({
 	transformJsonFromBackend: jest.fn(),
 }));
-jest.mock('../../components/UI/InfoCard/InfoCard', () => ({ title, children}) => (
-	<div data-testid='InfoCard'>
-		<div>{title}</div>
-		{children}
-	</div>
+jest.mock(
+	'../../components/UI/InfoCard/InfoCard',
+	() =>
+		({ title, children }) =>
+			(
+				<div data-testid='InfoCard'>
+					<div>{title}</div>
+					{children}
+				</div>
+			)
+);
+jest.mock(
+	'../../components/UI/LabelValuePair/LabelValuePair',
+	() =>
+		({ label, value }) =>
+			(
+				<div>
+					{label && <span>{label}</span>}
+					{value && <span>{value}</span>}
+				</div>
+			)
+);
+jest.mock('../../components/Loading/Loading', () => () => (
+	<div>Loading...</div>
 ));
-jest.mock('../../components/UI/LabelValuePair/LabelValuePair', () => ({ label, value }) => (
-	<div>
-		{label && <span>{label}</span>}
-		{value && <span>{value}</span>}
-	</div>
+jest.mock('../Application/Address/Address', () => () => (
+	<div>AddressComponent</div>
 ));
-jest.mock('../../components/Loading/Loading', () => () => <div>Loading...</div>);
-jest.mock('../Application/Address/Address', () => () => <div>AddressComponent</div>);
 jest.mock('antd', () => ({
-	Button: ({ children, ...mockProps }) => <button {...mockProps}>{children}</button>,
+	Button: ({ children, ...mockProps }) => (
+		<button {...mockProps}>{children}</button>
+	),
 	message: { error: jest.fn() },
 }));
-
 
 describe('ApplicantApplicationView component', () => {
 	beforeEach(() => {
@@ -61,18 +76,21 @@ describe('ApplicantApplicationView component', () => {
 	});
 
 	test('should render ApplicantApplicationView component for Stadtman application', async () => {
-		axios.get.mockResolvedValueOnce(mockStadtmanResponse)
+		axios.get.mockResolvedValueOnce(mockStadtmanResponse);
 		useParams.mockReturnValue({ appSysId: '12345' });
-		transformJsonFromBackend.transformJsonFromBackend.mockReturnValue(mockStadtmanApplication);
+		transformJsonFromBackend.transformJsonFromBackend.mockReturnValue(
+			mockStadtmanApplication
+		);
 
 		await act(async () => {
 			render(
 				<MemoryRouter initialEntries={['/apply/view/']}>
 					<ApplicationApplicationView {...mockProps} />
 				</MemoryRouter>
-			)
+			);
 		});
 
+		expect(screen.getByRole('button', { name: /Print/i })).toBeInTheDocument();
 		expect(screen.getByText(/Basic Information/i)).toBeInTheDocument();
 		expect(screen.getAllByText(/First Name/i).length).toBeGreaterThanOrEqual(1);
 		expect(screen.getAllByText(/Middle Name/i).length).toBeGreaterThanOrEqual(1);
@@ -83,6 +101,7 @@ describe('ApplicantApplicationView component', () => {
 		expect(screen.getByText(/Highest Level of Education/i)).toBeInTheDocument();
 		expect(screen.getByText(/US Citizen/i)).toBeInTheDocument();
 		expect(screen.getByText(/Focus Area/i)).toBeInTheDocument();
+		expect(screen.getByText(/AddressComponent/i)).toBeInTheDocument();
 		expect(screen.getByText(/References/i)).toBeInTheDocument();
 		expect(screen.getByText(/Phone Number/i)).toBeInTheDocument();
 		expect(screen.getByText(/Position Title/i)).toBeInTheDocument();
@@ -90,5 +109,23 @@ describe('ApplicantApplicationView component', () => {
 		expect(screen.getByText(/Relationship/i)).toBeInTheDocument();
 		expect(screen.getByText(/Is it okay for the Hiring Team to contact the reference directly?/i)).toBeInTheDocument();
 		expect(screen.getByText(/Documents/i)).toBeInTheDocument();
+	});
+
+	test('should render ApplicantApplicationView component for non-Stadtman application', async () => {
+		axios.get.mockResolvedValueOnce(mockResponse);
+		useParams.mockReturnValue({ appSysId: '12345' });
+		transformJsonFromBackend.transformJsonFromBackend.mockReturnValue(
+			mockApplication
+		);
+
+		await act(async () => {
+			render(
+				<MemoryRouter initialEntries={['/apply/view/']}>
+					<ApplicationApplicationView {...mockProps} />
+				</MemoryRouter>
+			);
+		});
+
+		expect(screen.queryByText(/Focus Area/i)).not.toBeInTheDocument();
 	});
 });
