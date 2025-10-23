e9335604d0f93164aa48efec7f46acf4b7a48b9a
Lechuga <lechugajm@nih.gov>
2025-10-23 11:50:23 -0500

    initial test file set up

---
+++ src/containers/ApplicantDashboard/ApplicantDashboard.test.js
@@ -0,0 +1,26 @@
+import ApplicantDashboard from './ApplicantDashboard';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('ApplicantDashboard', () => {
+    beforeEach(() => {
+        Object.defineProperty(window, 'matchMedia', {
+			writable: true,
+			value: jest.fn().mockImplementation((query) => ({
+				matches: false,
+				media: query,
+				onchange: null,
+				addListener: jest.fn(), // deprecated
+				removeListener: jest.fn(), // deprecated
+				addEventListener: jest.fn(),
+				removeEventListener: jest.fn(),
+				dispatchEvent: jest.fn(),
+			})),
+		});
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+});
