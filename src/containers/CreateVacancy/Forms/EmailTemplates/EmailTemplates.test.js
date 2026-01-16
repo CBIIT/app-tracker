3ecf006564a38dd5dd3566db8f70031d19d5d89f
Lechuga <lechugajm@nih.gov>
2026-01-16 14:10:38 -0600

    updated email template test file

--- src/containers/CreateVacancy/Forms/EmailTemplates/EmailTemplates.test.js
+++ src/containers/CreateVacancy/Forms/EmailTemplates/EmailTemplates.test.js
@@ -136,6 +136,15 @@ describe('EmailTemplates', () => {
 			);
 			expect(applicantReferenceReceivedApplicantHeader).toBeInTheDocument();
 			expect(applicantReferenceReceivedApplicantText).toBeInTheDocument();
+
+			const referenceRequestFromApplicantHeader = screen.getByText(
+				'Applicant Reference Request - Applicant'
+			)
+			const referenceRequestFromApplicantText = screen.getByText(
+				"<p>Dear Dr. #REF_FIRST_NAME# #REF_LAST_NAME#,</p><p>Dr. #APP_FIRST_NAME# #APP_LAST_NAME has applied to the Stadtman Investigator Program for a tenure-track investigator position (equivalent to assistant professor) in the Intramural Research Program at the National Institutes of Health and has provided your name as a reference. We would be grateful if you could submit a letter in support of the candidate’s application.</p><p>In your letter, please address the candidate's strengths and weaknesses, the potential for success in this position, and any other information you feel the search committee would find helpful in considering this application. All comments in your letter will be held confidential.</p><p><strong>The deadline for letter submission is #VACANCY_CLOSE_DATE# (11:59 pm EDT).<strong> Please upload your letter (either Word or PDF format) to our application system here. Once your letter has been uploaded, the candidate will be able to see that it has become part of their application package.</p><p>Information about the Stadtman Investigator Program can be found here.</p><p>Please contact us at stadtman@nih.gov if you have any questions or encounter any problems.</p><p>On behalf of the Intramural Research Program at the National Institutes of Health, thank you for your time and support.</p><p>Regards,</p><p>The Stadtman Investigator Program</p>"
+			)
+			expect(referenceRequestFromApplicantHeader).toBeInTheDocument();
+			expect(referenceRequestFromApplicantText).toBeInTheDocument();
 		});
 	});
 
@@ -156,6 +165,11 @@ describe('EmailTemplates', () => {
 			},
 			{
 				type: 'Applicant Reference Received - Applicant',
+				active: true,
+				text: 'Reference received applicant text',
+			},
+			{
+				type: 'Applicant Reference Request - Applicant',
 				active: true,
 				text: 'Reference received applicant text',
 			},
