752644ebeeb924100afb474f9d7abcec652e29bf
Jaya Chakladar <jaya.chakladar@nih.gov>
2026-01-27 12:16:43 -0700

    Add constant for logs

---
+++ src/utils/logging/logLevelConstants.js
@@ -0,0 +1,24 @@
+
+export const LogLevel = {
+    INFO: 'INFO',
+    WARN: 'WARN',
+    ERROR: 'ERROR',
+    DEBUG: 'DEBUG',
+};
+
+export const SENSITIVE_FIELDS = [
+    'password',
+    'token',
+    'apiKey',
+    'secret',
+    'authorization',
+    'creditCard',
+    'ssn',
+    'pin',
+    'privateKey',
+    'accessToken',
+    'refreshToken',
+    'bearerToken',
+];
+
+export default LogLevel;
