4c0c2849712fe9ddc65f8f0b5d4c7a1d7dfdafa7
Lechuga <lechugajm@nih.gov>
2026-03-03 15:25:10 -0600

    added test for Register Okta file

--- src/containers/RegisterOkta/RegisterOkta.test.js
+++ src/containers/RegisterOkta/RegisterOkta.test.js
@@ -1,13 +1,16 @@
 import RegisterOkta from "./RegisterOkta";
-import { MemoryRouter } from "react-router-dom";
 import useAuth from '../../hooks/useAuth';
+import axios from 'axios';
 import { REGISTER_OKTA } from "../../constants/Routes";
-import { render } from '@testing-library/react'
+import { CREATE_OKTA_USER } from "../../constants/ApiEndpoints";
+import { MemoryRouter } from "react-router-dom";
+import { render, fireEvent, screen, waitFor } from '@testing-library/react'
 
 jest.mock('../../hooks/useAuth', () => ({
     __esModule: true,
 	default: jest.fn(),
 }));
+jest.mock('axios');
 
 describe('RegisterOkta Component', () => {
     beforeAll(() => {
@@ -36,12 +39,43 @@ describe('RegisterOkta Component', () => {
         jest.clearAllMocks();
     });
 
-    test('Should render Register Okta page', () => {
+    test('Should submit valid form and shows success result', async () => {
+		axios.post.mockResolvedValue({
+			data: {
+				result: 'User account created, please check your email for a message from Okta to activate your account',
+			},
+		});
+
         render(
             <MemoryRouter initialEntries={[REGISTER_OKTA]} >
                 <RegisterOkta />
             </MemoryRouter>
         );
 
-    })
+		const nameInputs = screen.getAllByPlaceholderText('Please enter');
+		fireEvent.change(nameInputs[0], { target: { value: 'Jane' } });
+		fireEvent.change(nameInputs[1], { target: { value: 'Doe' } });
+
+		const emailInputs = screen.getAllByPlaceholderText('example@email.com');
+		fireEvent.change(emailInputs[0], { target: { value: 'jane@example.com' } });
+		fireEvent.change(emailInputs[1], { target: { value: 'jane@example.com' } });
+
+		fireEvent.change(screen.getByPlaceholderText('(123) 456-7890'), {
+			target: { value: '301-555-1212' },
+		});
+
+		fireEvent.click(screen.getByRole('button', { name: /create account/i }));
+
+		await waitFor(() => {
+			expect(axios.post).toHaveBeenCalledWith(CREATE_OKTA_USER, {
+				firstname: 'Jane',
+				lastname: 'Doe',
+				email: 'jane@example.com',
+        		confirmEmail: 'jane@example.com',
+        		phone: '301-555-1212',
+			});
+			expect(screen.getByText(/User account created, please check your email for a message from Okta/i)).toBeInTheDocument();
+		});
+
+    });
 })
